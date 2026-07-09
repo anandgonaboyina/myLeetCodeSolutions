c99b6e7bedb2d2c337746df868920f3ae8f20fc2
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-09 12:59:14 +0530

    Time: 0 ms (100%), Space: 8.6 MB (38.09%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        if(x<0) return false;
+        if(x <10) return true;
         int num =x; long long revNum=0;
         while(num>0)
         {
