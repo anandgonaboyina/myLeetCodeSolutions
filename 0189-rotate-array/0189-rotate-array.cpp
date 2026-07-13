51593fd7198fba906f9161ebdb8f1437e7abbbf6
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-13 18:53:49 +0530

    Time: 3 ms (8.31%), Space: 30.9 MB (5.29%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -5,55 +5,41 @@ public:
         k = k%n;
         if(k==0) return;
         vector<int> temp;
-        for(int i=n-k; i<n; i++)
+        for(int i=0; i<n; i++)
         {
-           temp.push_back(nums[i]);
-        }
-            for(int i=0; i<n-k; i++)
-        {
-           temp.push_back(nums[i]);
+           temp.push_back(nums[(n-k+i)%n]);
         }
         for(int i=0; i<n; i++)
         {
             nums[i]=temp[i];
         }
-        
     }
 };
 
 
 
-// class Solution {
-// public:
-//     void rotate(vector<int>& nums, int k) 
-//     {
-//         int m=nums.size();
-//         if(k%m==0) return;
-//         int nk = (k)%(m);
-//         vector<int> vt;
-//         for(int i=0; i<m; i++)
-//         {
-//             vt.push_back(nums[(m-nk+i)%m]);
-//         }
-//         for(int i=0; i<m; i++)
-//             nums[i] = vt[i];
-//     return;
-//     }
-// };
+
+
+
+
+
+
+
+
 
 // class Solution {
 // public:
 //     void rotate(vector<int>& nums, int k) 
 //     {
-//         int m=nums.size();
-//         if(k%m==0) return;
-//         int nk = (k)%(m);
+//         int n=nums.size();
+//         k = k%n;
+//         if(k==0) return;
 //         vector<int> vt;
-//         for(int i=0; i<m; i++)
+//         for(int i=0; i<n; i++)
 //         {
-//             vt.push_back(nums[(nk+i)%m]);
+//             vt.push_back(nums[(k+i)%n]);
 //         }
-//         for(int i=0; i<m; i++)
+//         for(int i=0; i<n; i++)
 //             nums[i] = vt[i];
 //     return;
 //     }
