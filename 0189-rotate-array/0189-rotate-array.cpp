de8f79d17b67cb66c36e9803e600205df5a54c81
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-13 19:57:01 +0530

    Time: 3 ms (8.31%), Space: 29.5 MB (69.41%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -15,48 +15,60 @@
 //         }
 //     }
 // };
-
+////////////////////////////////////////////////////////////////////////////
 
 // for extra space 1
 // Reverse the entire array completely.
 // Reverse the first k elements of that flipped array.
 // Reverse the remaining n - k elements.
 
+///manual reversing the array
+
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k)
+//      {
+//         int n = nums.size();
+//         k = k%n;
+//         if(k==0) return;
+//         for(int i=0; i<n/2; i++)
+//         {
+//                 swap(nums[i], nums[n-1-i]);
+//         }
+//         int left1=0, right1=k-1;
+//         while(left1<right1)
+//         {
+//                 swap(nums[left1], nums[right1]);
+//                 left1++; right1--;
+//         }
+//         int left2=k, right2=n-1;
+//         while(left2<right2)
+//         {
+//         swap(nums[left2], nums[right2]);
+//             left2++; right2--;
+//         }
+//     }
+//     };
+
+/////////////////////////////////////////////////////////////////////////////////
+
+
+//using the standard cpp library
 
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k)
-     {
+    void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k%n;
         if(k==0) return;
-        for(int i=0; i<n/2; i++)
-        {
-                swap(nums[i], nums[n-1-i]);
-        }
-        int left1=0, right1=k-1;
-        while(left1<right1)
-        {
-                swap(nums[left1], nums[right1]);
-                left1++; right1--;
-        }
-        int left2=k, right2=n-1;
-        while(left2<right2)
-        {
-        swap(nums[left2], nums[right2]);
-            left2++; right2--;
-        }
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
     }
-    };
+};
 
 
-
-
-
-
-
-
-
+////////////////////first method another formula to get backside equvalent/////////////////////
 // class Solution {
 // public:
 //     void rotate(vector<int>& nums, int k) 
