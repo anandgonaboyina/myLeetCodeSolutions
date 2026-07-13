ebfe82eba176a9bc88da1c53061d7a82db375669
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-13 19:41:25 +0530

    Time: 0 ms (100%), Space: 29.7 MB (45.75%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,23 +1,53 @@
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         k = k%n;
+//         if(k==0) return;
+//         vector<int> temp;
+//         for(int i=0; i<n; i++)
+//         {
+//            temp.push_back(nums[(n-k+i)%n]);
+//         }
+//         for(int i=0; i<n; i++)
+//         {
+//             nums[i]=temp[i];
+//         }
+//     }
+// };
+
+
+// for extra space 1
+// Reverse the entire array completely.
+// Reverse the first k elements of that flipped array.
+// Reverse the remaining n - k elements.
+
+
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
+    void rotate(vector<int>& nums, int k)
+     {
         int n = nums.size();
         k = k%n;
         if(k==0) return;
-        vector<int> temp;
-        for(int i=0; i<n; i++)
+        for(int i=0; i<n/2; i++)
         {
-           temp.push_back(nums[(n-k+i)%n]);
+                swap(nums[i], nums[n-1-i]);
         }
-        for(int i=0; i<n; i++)
+        int left1=0, right1=k-1;
+        while(left1<right1)
         {
-            nums[i]=temp[i];
+                swap(nums[left1], nums[right1]);
+                left1++; right1--;
+        }
+        int left2=k, right2=n-1;
+        while(left2<right2)
+        {
+        swap(nums[left2], nums[right2]);
+            left2++; right2--;
         }
     }
-};
-
-
-
+    };
 
 
 
