e0e9b4ca7b287c7346dea09fe6c23203b592070e
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-14 14:54:23 +0530

    Time: 0 ms (100%), Space: 14.8 MB (37.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,3 +1,5 @@
+//Brute force solution
+/*
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -13,12 +15,37 @@ public:
         return {-1,-1};
     }
 };
+*/
+/////////////////////////////////////////////////////////////////////
+// Better Approach for unsorted array
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        unordered_map<int, int> mp;
+        int n=nums.size();
+        for(int i=0; i<n; i++)
+        {
+            if(mp.find(target-nums[i]) != mp.end())
+                return {mp[target-nums[i]], i};
+            mp[nums[i]]=i;
+        }
+        return {-1,-1};
+    }
+};
 
 
 
 
 
-/*
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////
+/* previously did
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target)
