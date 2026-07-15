dd11c87e0d22cf105131b4abb43e2d60a0c7d699
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-15 16:22:28 +0530

    Add solution for Left Rotate Array by K Places

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Left-Rotate-Array-by-K-Places/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void rotateArray(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        if(k==0) return;
+            reverse(nums.begin(), nums.end());
+            reverse(nums.begin(), nums.begin()+n-k);
+            reverse(nums.begin()+n-k, nums.end());
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+//
