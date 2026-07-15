76b27aa6b89ffb611c6ef53ef77070c89fe3bfc2
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-15 18:37:00 +0530

    Add solution for Best time to buy and sell stock

--- 0000-Strivers-A2Z-DSA-Sheet/Best-time-to-buy-and-sell-stock/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,8 +1,34 @@
-class Solution{
+//brute force solution but problem is TLE for array of 10^5 size
+/*
+class Solution {
 public:
-    int stockBuySell(vector<int> arr, int n)
+    int maxProfit(vector<int>& prices)
     {
-        //solved directly in leet code
+        int n = prices.size(), maxProfit=0, profit=0;
+        for(int i=0; i<n-1; i++)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                profit=prices[j]-prices[i];
+                maxProfit= max(maxProfit, profit);
+            }
+        }
+        return maxProfit;
     }
 };
-
+*/
+//////////////////////////////////////
+//better approach
+class Solution {
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        int minp=prices[0], maxp=0;
+        for(int i=0; i<prices.size(); i++)
+       {
+            minp = min(minp, prices[i]);
+            maxp = max(maxp, prices[i]-minp);
+       }
+       return maxp;
+    }
+};
