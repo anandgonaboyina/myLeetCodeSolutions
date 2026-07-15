f1d716cc098eeba0fff01f531d06960ae3e32a6c
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-15 16:19:02 +0530

    Add solution for Highest Occurring Element in an Array

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Highest-Occurring-Element-in-an-Array/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mostFrequentElement(vector<int>& nums) {
+        if(nums.empty()) return -1;
+        unordered_map<int, int> mp;
+        pair<int,int> maxCnt;
+        maxCnt = {0,0};
+        for(int i=0; i<nums.size(); i++)
+        {
+            mp[nums[i]]++;
+            if(mp[nums[i]]>maxCnt.second)
+                maxCnt = {nums[i], mp[nums[i]]};
+            else if(mp[nums[i]]==maxCnt.second && nums[i]<maxCnt.first)
+                maxCnt = {nums[i], mp[nums[i]]};
+        }
+
+    return maxCnt.first;
+    }
+};
