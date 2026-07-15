250e65441d8813135dc3778bf4229db28911efbd
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-15 19:36:42 +0530

    Time: 4 ms (77.46%), Space: 128.3 MB (78.02%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -3,37 +3,55 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) 
     {
-        vector<int> Pnums, Nnums;
         int n = nums.size();
+        vector<int> ans(n,0);
+        int posInd=0, negInd=1;
         for(int i=0; i<n; i++)
         {
             if(nums[i]>0)
             {
-                Pnums.push_back(nums[i]);
+                ans[posInd]=nums[i];
+                posInd +=2;
             }
             else
             {
-                 Nnums.push_back(nums[i]);
+                 ans[negInd] = nums[i];
+                 negInd +=2;
             }
         }
-        int p=0,q=0;
-        for(int i=0; i<n; i++)
-        {
-            if(i%2 ==0)
-                nums[i]=Pnums[p++];
-            else
-                nums[i]=Nnums[q++];
-        }
-        return nums;
+        return ans;
     }
 };
 
 
-/*
-class Solution {
-public:
-    vector<int> rearrangeArray(vector<int>& nums) {
-        
-    }
-};
-*/
+// better approach to make sc as 1
+// class Solution {
+// public:
+//     vector<int> rearrangeArray(vector<int>& nums) 
+//     {
+//         int first=0, second=1;
+//         int n=nums.size();
+//         int i=1;
+//                     while(nums[0] <0 && i <n)
+//                 {
+//                     if(nums[i]>0)
+//                     swap(nums[0], nums[i]);
+//                     i++;
+//                 }
+//             i=0;
+//         for(i; i<n; i++)
+//         {
+//             if(nums[i]>0)
+//                 {
+//                     swap(nums[first], nums[i]);
+//                     first +=2;
+//                 }
+//             else
+//                 {
+//                     swap(nums[second], nums[i]);
+//                     second +=2;
+//                 }
+//         }
+//         return nums;
+//     }
+// };
