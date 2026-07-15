071ee171f8e2ec48c702b70e6cbb6ba09af26fd8
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-15 16:20:04 +0530

    Add solution for Recursive Insertion Sort

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Recursive-Insertion-Sort/solution.cpp
@@ -0,0 +1,37 @@
+// class Solution {
+// public:
+//     vector<int> insertionSort(vector<int>& nums) {
+//         int n =nums.size();
+//         for(int i=1; i<n; i++)
+//         {
+//             int key = nums[i];
+//             int j=i-1;
+//             while(j>=0 && nums[j]>key)
+//             {
+//                 nums[j+1] = nums[j];
+//                 j--;
+//             }
+//             nums[j+1] = key;
+//         }
+//         return nums;
+//     }
+// };
+
+//direct swap without empty room
+class Solution {
+public:
+    vector<int> insertionSort(vector<int>& nums) {
+        int n =nums.size();
+        for(int i=1; i<n; i++)
+        {
+            int key = nums[i];
+            int j=i-1;
+            while(j>=0 && nums[j]>key)
+            {
+                swap(nums[j], key)
+                j--;
+            }
+        }
+        return nums;
+    }
+};
