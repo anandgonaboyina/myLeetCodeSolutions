78d06dff219cd85f8eec91c84e60601dcc5d9db4
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-15 19:46:22 +0530

    Add solution for Rearrange array elements by sign

--- 0000-Strivers-A2Z-DSA-Sheet/Rearrange-array-elements-by-sign/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Rearrange-array-elements-by-sign/solution.cpp
@@ -1,39 +1,55 @@
-//Brute force solution
+//brute force solution
 /*
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> rearrangeArray(vector<int>& nums) 
+    {
+        vector<int> Pnums, Nnums;
         int n = nums.size();
         for(int i=0; i<n; i++)
         {
-        for(int j=i+1; j<n; j++)
+            if(nums[i]>0)
+            {
+                Pnums.push_back(nums[i]);
+            }
+            else
+            {
+                 Nnums.push_back(nums[i]);
+            }
+        }
+        int p=0,q=0;
+        for(int i=0; i<n; i++)
         {
-            if(nums[i]+nums[j]==target)
-            return {i,j};
+                nums[i]=Pnums[p++];
+                i++;
+                nums[i]=Nnums[q++];
         }
-        }
-        return {-1,-1};
+        return nums;
     }
 };
 */
-/////////////////////////////////////////////////////////////////////
-//////////////if array is sorted we can use two pointer approach
-// as we know if new element is bigger or smaller
-// we can get extra space as 1 with tc N
 
-// Better Approach for unsorted array tc is N
+// Optimal Approach to make it in single pass
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) 
+    vector<int> rearrangeArray(vector<int>& nums) 
     {
-        unordered_map<int, int> mp;
-        int n=nums.size();
+        int n = nums.size();
+        vector<int> ans(n);
+        int posInd=0, negInd=1;
         for(int i=0; i<n; i++)
         {
-            if(mp.find(target-nums[i]) != mp.end())
-                return {mp[target-nums[i]], i};
-            mp[nums[i]]=i;
+            if(nums[i]>0)
+            {
+                ans[posInd]=nums[i];
+                posInd +=2;
+            }
+            else
+            {
+                 ans[negInd] = nums[i];
+                 negInd +=2;
+            }
         }
-        return {-1,-1};
+        return ans;
     }
 };
