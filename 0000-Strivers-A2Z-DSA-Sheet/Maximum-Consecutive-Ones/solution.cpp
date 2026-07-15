717cf679d1b256492c3fcf55038a19d31097aff4
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-15 16:24:13 +0530

    Add solution for Maximum Consecutive Ones

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Maximum-Consecutive-Ones/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+                int maxOnes =0;
+        int n =nums.size(), cnt=0;
+        for(int i=0; i<n; i++)
+            {
+                if(nums[i]==1)
+                {
+                    cnt++;
+                    maxOnes = max(maxOnes, cnt);
+                }
+                else
+                    cnt=0;
+            }
+        return maxOnes;
+    }
+};
