fb846d0214607c796a54445a0c97757a3202f7a6
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-15 16:21:17 +0530

    Add solution for Kadane's Algorithm

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Kadane-s-Algorithm/solution.cpp
@@ -0,0 +1,63 @@
+//brute force
+/*
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) 
+    {   
+        int sum=0, n=nums.size();
+        int maxSum=INT_MIN;
+        for(int i=0; i<n; i++)
+        {
+            sum=0;
+            for(int j=i;j<n; j++ )
+            {
+                sum +=nums[j];
+                maxSum = max(maxSum, sum);
+            }
+        }
+        return maxSum;
+    }
+};
+*/
+///////////////////////////////////////////////////////
+// Optimal approach
+/*
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums)
+    {
+        int maxSum=INT_MIN, sum=0;
+        for(int x : nums)
+        {
+            sum +=x;
+            maxSum = max(maxSum, sum);
+            if(sum<0)
+                sum=0;
+        }
+        return maxSum;
+    }
+};
+
+*/
+/////////////////////////////////////////////////////////////
+//FOR Returning the subArray that has maximum sum
+
+class Solution {
+public:
+    vector<int> maxSubArray(vector<int>& nums) 
+    {
+        int sum=0, maxSum=INT_MIN;
+        vector<int> vt;
+        for(int x : nums)
+        {
+            sum +=x;
+            maxSum = max(maxSum, sum);
+            if(sum<0)
+            {
+                sum=0;
+            }
+            vt.push_back(x);
+        }
+    }
+};
+//anandkumar
