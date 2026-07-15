a2a93c9f611cec4a6b1cc57745ab69d1bc064d64
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-15 12:40:13 +0530

    Sync Striver Solution - LeetHub

--- 0000-Strivers-A2Z-DSA-Sheet/Reverse a number/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Reverse a number/solution.cpp
@@ -1,4 +1,27 @@
+class Solution {
+public:
+    int reverseNumber(int n) {
+        int revN=0, num=n;
+        while(num>0)
+        {
+            int ld = num%10;
+            num=num/10;
+            revN =revN*10 + ld;
+        }
+        return revN;
+    }
 };
-    }
-        return revN;
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
