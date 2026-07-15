8f505618a1da68161d2ad809216d84eb943f6daf
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-15 13:09:36 +0530

    Sync Striver Solution - LeetHub

--- 0000-Strivers-A2Z-DSA-Sheet/Union of two sorted arrays/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Union of two sorted arrays/solution.cpp
@@ -4,12 +4,12 @@
 //             temp.push_back(it);
 //         /*
 //         vector<int> temp;
-//         st.insert(x);
-//         for(int x : nums2)
-//         st.insert(x);
 time and
 //         // total TC is for n=n1+n2 O(nlogn)
 //         for(int x : nums1)
+//         st.insert(x);
+//         for(int x : nums2)
+//         st.insert(x);
 //         set<int> st;  
 //         // it is ordered set for each insertion takes O(logn) 
 //     vector<int> unionArray(vector<int>& nums1, vector<int>& 
