5273417c01378f82b069e19c7062e9ca810f5f14
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-15 16:19:20 +0530

    Add solution for Selection Sort

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Selection-Sort/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> selectionSort(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0; i<n-1; i++)
+        {
+            int min=i;
+            for(int j=i+1; j<n; j++)
+            {
+                if(nums[j]<nums[min])
+                    min=j;
+            }
+            if(min !=i)
+                swap(nums[i], nums[min]);
+        }
+        return nums;
+    }
+};
