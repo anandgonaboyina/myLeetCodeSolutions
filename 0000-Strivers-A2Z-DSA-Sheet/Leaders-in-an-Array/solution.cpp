d1cf0ec3e68f088eb6ae211071cba49bcb259cfe
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-16 19:55:43 +0530

    Add solution for Leaders in an Array

--- 0000-Strivers-A2Z-DSA-Sheet/Leaders-in-an-Array/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Leaders-in-an-Array/solution.cpp
@@ -1,23 +1,78 @@
-//my brute force solution
+// class Solution {
+// public:
+//     vector<int> unionArray(vector<int>& nums1, vector<int>& nums2)
+//     {
+//         set<int> st;  
+//         // it is ordered set for each insertion takes O(logn) time and
+//         // total TC is for n=n1+n2 O(nlogn)
+//         for(int x : nums1)
+//         st.insert(x);
+//         for(int x : nums2)
+//         st.insert(x);
+//         /*
+//         vector<int> temp;
+//                 for(auto it : st)
+//         {
+//             temp.push_back(it);
+//         }
+//         return temp; 
+//         */
+//         //or
+//         return vector<int>(st.begin(), st.end());
+//     }
+// };
+
+// if order not important we can use unordered_set as it takes insertion time O(1) as it uses hash table
+// just initialize above one as below
+//  unordered_set<int> st;
+
+//Optimal approach
+
 class Solution {
 public:
-    vector<int> leaders(vector<int>& nums) {
-      if(n<2) return nums;
-      vector<int> ans; int n=nums.size();
-      for(int i=0; i<n; i++)
-      {
-        bool isLeader = true;
-        for(int j=i+1; j<n; j++)
+    vector<int> unionArray(vector<int>& nums1, vector<int>& nums2)
+    {
+        if(nums1.empty()) return nums2;
+        if(nums2.empty()) return nums1;
+        int left=0, right=0;
+        int n1=nums1.size(), n2=nums2.size();
+
+        vector<int> ans;
+        while(left<n1 && right<n2)
         {
-            if(nums[j]>nums[i])
+            if(nums1[left]==nums2[right])
                 {
-                    isLeader = false;
-                    break;
+                    if(ans.empty() || ans.back() != nums1[left])
+                        ans.push_back(nums1[left]);
+                    left++; right++;
+                }
+            else if(nums1[left]<nums2[right])
+                {
+                    if(ans.empty() || ans.back() != nums1[left])
+                        ans.push_back(nums1[left]);
+                    left++;
+                }
+            else
+                {
+                    if(ans.empty() || ans.back() != nums2[right])
+                        ans.push_back(nums2[right]);
+                    right++;
                 }
         }
-        if(isLeader)
-            ans.push(nums[i]);
-      }
-      return ans;
+
+        while(left<n1)
+        {
+            if(ans.back() != nums1[left])
+                ans.push_back(nums1[left]);
+            left++;
+        }
+        while(right<n2)
+        {
+            if(ans.back() != nums2[right])
+                ans.push_back(nums2[right]);
+            right++;
+        }
+
+        return ans;
     }
 };
