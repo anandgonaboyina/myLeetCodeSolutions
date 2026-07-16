87d8defdc9390dbfbad8e2389412a5dad56d592c
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-16 22:17:26 +0530

    Time: 2 ms (31.31%), Space: 21.5 MB (12.91%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -5,26 +5,28 @@ public:
     {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<int> zeroRows, zeroCols;
+        unordered_set<int> zeroRows, zeroCols;
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)
             {
                 if(matrix[i][j]==0)
                     {
-                        zeroRows.push_back(i);
-                        zeroCols.push_back(j);
+                        zeroRows.insert(i);
+                        zeroCols.insert(j);
                     }
             }
         }
         int k = zeroRows.size();
-        for(int i=0; i<k; i++)
+        for(int row : zeroRows)
         {
-            matrix[zeroRows[i]] = vector<int>(n, 0);
-            for(int j=0; j<m; j++)
-            {
-                matrix[j][zeroCols[i]]=0;
-            }
+            matrix[row] = vector<int>(n, 0);
         }
+            for(int col : zeroCols)
+            {
+                for(int i=0; i<m; i++)
+                    matrix[i][col]=0;
+            }
+        
     }
 };
