d7db04d1536879f597fbe463e82ecfd08dfa4459
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-16 22:25:06 +0530

    Time: 0 ms (100%), Space: 21.4 MB (14.36%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,4 +1,39 @@
 //my brute force solution
+/*
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) 
+    {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> zeroRows, zeroCols;
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(matrix[i][j]==0)
+                    {
+                        zeroRows.push_back(i);
+                        zeroCols.push_back(j);
+                    }
+            }
+        }
+        int k = zeroRows.size();
+        for(int i=0; i<k; i++)
+        {
+            matrix[zeroRows[i]] = vector<int>(n, 0);
+            for(int j=0; j<m; j++)
+            {
+                matrix[j][zeroCols[i]]=0;
+            }
+        }
+    }
+};
+*/
+//in above i used vectors those allows multiple same values of i and j so
+//as no need to make the row again to zero vector as it takes time i used set to make it take unique i values not duplicate and for columns too 
+
+//my better solution
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) 
@@ -22,7 +57,7 @@ public:
         {
             matrix[row] = vector<int>(n, 0);
         }
-            for(int col : zeroCols)
+        for(int col : zeroCols)
             {
                 for(int i=0; i<m; i++)
                     matrix[i][col]=0;
