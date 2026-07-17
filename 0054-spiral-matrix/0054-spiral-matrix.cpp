a5507e858339f61671d9179edbff0623d4083a34
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-17 07:45:13 +0530

    Time: 0 ms (100%), Space: 9.5 MB (16.55%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,3 +1,5 @@
+//my brute force solution  with 100% beats 
+/*
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) 
@@ -5,13 +7,6 @@ public:
         int m=matrix.size();
         int n=matrix[0].size();
         if(m<2) return matrix[0];
-        // if(m==2)
-        // {
-        //     vector<int>ans;
-        //     ans.insert(ans.end(), matrix[0].begin(), matrix[0].end());
-        //     ans.insert(ans.end(), matrix[1].begin(), matrix[1].end());
-        //     return ans;
-        // }
         vector<int>ans;
         int istart=0,jstart=0;
         while(true)
@@ -41,4 +36,49 @@ public:
     }
 
 
+};
+
+*/
+//above Input Mutation: we are using reverse() directly on matrix
+//if we are not allowed to change the matrix as it asked to traversal but we used reversal on matirx and cant make copy as it increases space complexity
+/*it was not good as it does these below things
+Calling reverse() directly on the rows permanently alters the input matrix. If another part of the software needed to read that matrix after your function ran, it would read corrupted data.
+Fragility: The dense block of nested if statements is very hard to maintain
+*/
+///////////////////////////////////////////////
+//better approach
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) 
+    {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        if(m<2) return matrix[0];
+        vector<int>ans;
+        int top=0,bottom=m-1;
+        int left=0,right=n-1;
+        while(top<=bottom && left <=right)
+     {   
+        for(int i=left; i<=right; i++)
+        {
+                ans.push_back(matrix[top][i]);
+        }
+            top++;
+        for(int i=top; i<=bottom; i++)
+            {
+                ans.push_back(matrix[i][right]);
+            }
+        right--;
+        if(top<=bottom)
+{        for(int i=right; i>=left; i--)
+            ans.push_back(matrix[bottom][i]);
+        bottom--;}
+        if(left<=right)
+{        for(int i=bottom; i>=top; i--)
+            ans.push_back(matrix[i][left]);
+        left++;}
+     }
+     return ans;
+    }
 };
