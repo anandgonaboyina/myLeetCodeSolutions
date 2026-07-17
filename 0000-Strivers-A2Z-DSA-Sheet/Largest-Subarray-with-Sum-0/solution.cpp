c9fddd08e7e66214a45f7e86138bee2ebf1bdb50
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-17 17:13:39 +0530

    Add solution for Largest Subarray with Sum 0

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Largest-Subarray-with-Sum-0/solution.cpp
@@ -0,0 +1,22 @@
+//my optimal solution
+class Solution {
+  public:
+    int maxLen(vector<int>& arr)
+    {   int n =arr.size();
+        unordered_map<int, int>mp;
+        int maxL=0, sum=0;
+        for(int i =0; i<n; i++)
+        {
+            sum += arr[i];
+            if(sum==0)
+                maxL = i+1;
+            else if(mp.find(sum-0) !=mp.end())
+                {
+                    maxL = max(maxL, i-mp[sum]+1);
+                }
+        if(mp.find(sum) == mp.end())
+            mp[sum]=i;
+        }
+        return maxL;
+    }
+};
