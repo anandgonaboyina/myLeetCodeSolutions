b38085f2ee1274cafaa6814b25d330daf5bb4d2e
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-17 19:52:25 +0530

    Add solution for Merge two sorted arrays without extra space

--- 0000-Strivers-A2Z-DSA-Sheet/Merge-two-sorted-arrays-without-extra-space/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Merge-two-sorted-arrays-without-extra-space/solution.cpp
@@ -1,49 +1,12 @@
 //my brute force solution
-/*
 class Solution {
 public:
-    vector<int> leaders(vector<int>& nums) {
-    int n=nums.size();
-      if(n<2) return nums;
-      vector<int> ans;
-      for(int i=0; i<n; i++)
-      {
-        bool isLeader = true;
-        for(int j=i+1; j<n; j++)
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+    {
+        for(int i=0; i<n; i++)
         {
-            if(nums[j]>nums[i])
-                {
-                    isLeader = false;
-                    break;
-                }
+            nums1[m+i]=nums2[i];
         }
-        if(isLeader)
-            ans.push_back(nums[i]);
-      }
-      return ans;
+        sort(nums1.begin(), nums1.end());
     }
 };
-*/
-
-//my Optimal approach
-
-class Solution {
-public:
-    vector<int> leaders(vector<int>& nums) {
-        int n=nums.size();
-      if(n<2) return nums;
-      int maxN=INT_MIN;
-      vector<int> ans; int n=nums.size();
-      for(int i=n-1; i>=0; i--)
-      {
-        if(nums[i]>maxN )
-        {
-            maxN =max(nums[i], maxN);
-            ans.push_back(nums[i]);
-        }
-      };
-      reverse(ans.begin(), ans.end());
-      return ans;
-      }
-    };
-
