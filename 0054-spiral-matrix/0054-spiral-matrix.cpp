f9f5a63e6b3bc300f31ac6e34ba04e406db48f23
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-17 07:50:09 +0530

    Time: 0 ms (100%), Space: 9.2 MB (98.72%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -64,20 +64,22 @@ public:
         {
                 ans.push_back(matrix[top][i]);
         }
-            top++;
+        top++;
         for(int i=top; i<=bottom; i++)
             {
                 ans.push_back(matrix[i][right]);
             }
         right--;
         if(top<=bottom)
-{        for(int i=right; i>=left; i--)
-            ans.push_back(matrix[bottom][i]);
-        bottom--;}
+        {   for(int i=right; i>=left; i--)
+                ans.push_back(matrix[bottom][i]);
+            bottom--;
+        }
         if(left<=right)
-{        for(int i=bottom; i>=top; i--)
-            ans.push_back(matrix[i][left]);
-        left++;}
+        {   for(int i=bottom; i>=top; i--)
+                ans.push_back(matrix[i][left]);
+            left++;
+        }
      }
      return ans;
     }
