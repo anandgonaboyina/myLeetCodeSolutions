ec8ed006741f9ed671c582bed1447b3fdf9a6be0
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-17 15:45:20 +0530

    Time: 46 ms (68.67%), Space: 29.2 MB (47.49%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,3 +1,75 @@
+// my brute force solution
+/*
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) 
+    {
+        int n = nums.size();
+        set <vector<int>> ans;
+        for(int i=0; i<n-2; i++)
+        {
+            for(int j=i+1; j<n-1; j++)
+        {
+            for(int k=j+1; k<n; k++)
+        {
+            if(i != j && i != k && j !=k && nums[i]+nums[j]+nums[k]==0)
+               { 
+                vector <int> triplet={nums[i], nums[j], nums[k]};
+                sort(triplet.begin(), triplet.end());
+                ans.insert(triplet);
+                }
+        }
+        }
+        }
+        return vector<vector<int>>(ans.begin(), ans.end());
+    }
+};
+*/
+//optimal approach
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) 
+    {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        for(int i=0; i<n; i++)
+        {
+            while(i>0 && i<n && nums[i]==nums[i-1])
+                i++;
+            int left = i+1, right=n-1;
+            while(left < right)
+        {   int sum=nums[i]+nums[left]+nums[right];                        
+            if(sum<0)
+               {
+                    left++;
+               }
+            else if(sum>0)
+                right--;
+            else
+                {
+                ans.push_back({nums[i], nums[left], nums[right]});
+                left++; right--;
+                while(left < right && nums[left] == nums[left-1])
+                left++;
+                while(left<right &&  nums[right] == nums[right+1])
+                right--;
+                }
+        }
+        }
+        
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+// previous approach
+/*
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) 
@@ -32,3 +104,6 @@ public:
         return sol;
     }
 };
+
+
+*/
