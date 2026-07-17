9cc3964c2cc76cddfd5c72fa81aaa002ce3fb3fb
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-17 21:45:57 +0530

    Add solution for Search X in sorted array

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Search-X-in-sorted-array/solution.cpp
@@ -0,0 +1,22 @@
+//leetcode problem : 704 binary search
+//my brute force solution
+class Solution {
+public:
+    int search(vector<int>& nums, int target)
+     {
+        int n = nums.size();
+        int i=0;
+        int j=n-1;
+        while(i<=j)
+        {
+            int m = (i+j)/2;
+            if(nums[m]== target)
+                return m;
+            else if(target>nums[m])
+                i=m+1;
+            else if(target<nums[m])
+                j=m-1;
+        }
+        return -1;
+    }
+};
