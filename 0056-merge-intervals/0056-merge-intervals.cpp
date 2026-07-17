837fa2a043da46bf3ac8b92f6b835bcb71c055b4
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-17 18:41:28 +0530

    Time: 4 ms (66.01%), Space: 23.7 MB (82.45%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,3 +1,7 @@
+//my optimal brute force solution but modifies the input intervals
+// so look at another solution implementation of same approach with less confusion without nested ifs
+/*
+/*
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) 
@@ -17,6 +21,28 @@ public:
         }
         else
         ans.push_back(intervals[i]);
+        }
+        return ans;
+    }
+};
+*/
+// ***same solution without modify the given intervals with less confusion
+//and in less lines
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) 
+    {
+        int n= intervals.size();
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+        for(int i=1;i<n; i++)
+        {
+            if(ans.back()[1]>=intervals[i][0])
+                ans.back()[1]=max(ans.back()[1], intervals[i][1]);
+            else
+                ans.push_back(intervals[i]);
         }
         return ans;
     }
