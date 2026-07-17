c02c034aeec38dbb49e6f9a9aa26597043ab1d9a
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-17 11:11:09 +0530

    Time: 0 ms (100%), Space: 9.3 MB (14.93%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,3 +1,5 @@
+//my brute force solution
+
 class Solution {
 public:
     vector<int> getRow(int rowIndex)
@@ -15,3 +17,23 @@ public:
         return pasTri[rowIndex];
     }
 };
+
+// optimal solution will see later not done yet
+/*
+class Solution {
+public:
+    vector<int> getRow(int rowIndex)
+    {
+        <vector<int>row(0, 1);
+        if(rowIndex==0) return row;
+        for(int i=1; i<=rowIndex; i++)
+        {
+            for(int j=i-1; j>0; j++)
+            {
+                row[j] = row[j-1] + pasTri[i-1][j];
+            }
+        }
+        return row;
+    }
+};
+*/
