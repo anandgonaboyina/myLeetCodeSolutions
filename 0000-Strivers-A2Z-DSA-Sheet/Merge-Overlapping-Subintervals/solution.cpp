2b738b437bd041085f456f667daf25eee7553ab5
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-17 18:42:12 +0530

    Add solution for Merge Overlapping Subintervals

--- 0000-Strivers-A2Z-DSA-Sheet/Merge-Overlapping-Subintervals/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Merge-Overlapping-Subintervals/solution.cpp
@@ -35,11 +35,11 @@ public:
         int n= arr.size();
         sort(arr.begin(), arr.end());
         vector<vector<int>> ans;
-        ans.push_back(arr[0])
+        ans.push_back(arr[0]);
         for(int i=1;i<n; i++)
         {
             if(ans.back()[1]>=arr[i][0])
-                ans.back()[1]=max(ans.back()[1], arr[i][1])
+                ans.back()[1]=max(ans.back()[1], arr[i][1]);
             else
                 ans.push_back(arr[i]);
         }
