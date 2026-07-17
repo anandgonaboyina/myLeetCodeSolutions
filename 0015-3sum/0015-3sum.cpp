44dc48a666f8c740b97db553c619c7901787e066
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-17 15:50:31 +0530

    Time: 51 ms (43.57%), Space: 29 MB (73.4%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -35,8 +35,8 @@ public:
         vector<vector<int>> ans;
         for(int i=0; i<n; i++)
         {
-            while(i>0 && i<n && nums[i]==nums[i-1])
-                i++;
+            if(i>0 && nums[i]==nums[i-1])
+                continue; // skiping the duplicate value
             int left = i+1, right=n-1;
             while(left < right)
         {   int sum=nums[i]+nums[left]+nums[right];                        
@@ -57,53 +57,6 @@ public:
                 }
         }
         }
-        
         return ans;
     }
 };
-
-
-
-
-
-
-
-// previous approach
-/*
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) 
-    {
-        vector<vector<int>> sol;
-        sort(nums.begin(), nums.end());
-        int n= nums.size();
-        for(int i=0; i<n; i++)
-           { 
-            if(i>0 && nums[i]==nums[i-1])
-                continue;
-            int left = i+1, right =n-1;
-                while(left<right)
-                {
-                        int sum =nums[i]+nums[right]+nums[left];
-                        if(sum<0)
-                            left++;
-                        else if(sum>0)
-                            right--;
-                        else
-                        {
-                                sol.push_back({nums[i], nums[left], nums[right]});
-                                while(left<right && nums[left] == nums[left+1])
-                                    left++;
-                                while(left<right && nums[right] == nums[right-1])
-                                    right--;
-                        left++; right--;
-                        }
-                                        
-                }
-           }
-        return sol;
-    }
-};
-
-
-*/
