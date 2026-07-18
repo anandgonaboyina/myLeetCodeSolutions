c1f659635943d5839b122765247bea2c408ca988
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-18 12:46:39 +0530

    Time: 0 ms (100%), Space: 31.2 MB (99.19%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,4 +1,6 @@
 //my brute force solution is optimal solution
+//iterative method
+/*
 class Solution {
 public:
     int search(vector<int>& nums, int target)
@@ -18,5 +20,30 @@ public:
                 j=m-1;
         }
         return -1;
+    }
+};
+
+*/
+
+//recursive method
+class Solution {
+public:
+    int bSearch(vector<int>&nums, int left, int right, int target)
+    {
+        if(left>right) return -1;
+        int mid = left + (right-left)/2;
+        if(target==nums[mid])
+            return mid;
+        else if(target<nums[mid])
+            return bSearch(nums, left, mid-1, target);
+        else
+            return bSearch(nums, mid+1, right, target);
+    }
+
+    int search(vector<int>& nums, int target)
+     {
+        int n = nums.size();
+            int res = bSearch(nums, 0, n-1, target);
+        return res;
     }
 };
