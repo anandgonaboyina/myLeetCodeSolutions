2368b8271f03d98886a3494a18079add8bdc6376
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-18 13:57:44 +0530

    Time: 0 ms (100%), Space: 13.5 MB (78.17%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,4 +1,43 @@
-//my brute force solution is optimal solution
+//bineary search solution
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) 
+    {
+        int n = nums.size();
+        int left=0, right=n-1;
+        int ans;
+        while(left<=right)
+        {
+            int mid = right + (left-right)/2;
+            if(target==nums[mid])
+                {
+                    ans=mid; break;
+                }
+            else if(target<nums[mid])
+                    {
+                        right = mid-1; ans=mid;
+                    }
+            else if(target>nums[mid])
+                {
+                    left = mid+1; ans=left;
+                }
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+//my brute force solution is leanear searcg solution => Not a binary search
+/*
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target)
@@ -25,3 +64,5 @@ public:
         return -1; //just to skip compilation error
     }
 };
+
+*/
