a08a932393e5a9414d8f05dcd97b60f78e26801f
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-18 13:35:44 +0530

    Add solution for Upper Bound

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Upper-Bound/solution.cpp
@@ -0,0 +1,36 @@
+//leetcode problem : 35 Search Insert Position
+// class Solution{
+// public:
+//     int lowerBound(vector<int> &nums, int x)
+//     {
+        
+//     }
+// };
+
+//my brute force solution is optimal solution
+class Solution {
+public:
+    int lowerBound(vector<int>& nums, int target)
+    {
+        int n = nums.size();
+        int left=0, right=n-1;
+        int p=0;
+        while(p<n)
+        {
+            if(target==nums[left])
+                return left;
+            else if(target==nums[right])
+                return right;
+            else if(target<nums[left])
+                return left;
+            else if(target>nums[right])
+                return right+1;
+            else 
+                {
+                    left++; right--;
+                }
+            p++;
+        }
+        return -1; //just to skip compilation error
+    }
+};
