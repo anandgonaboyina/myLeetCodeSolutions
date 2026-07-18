8eae64cecb5887130d6db45ce7444b073fedb692
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-18 18:17:05 +0530

    Time: 0 ms (100%), Space: 17.7 MB (19.97%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,96 @@
+// binary search solution
+class Solution {
+public:
+    int firstInd(vector<int> &nums, int target)
+    {
+        int left = 0, right= nums.size()-1;
+        int first=-1;
+        while(left <= right)
+        {
+            int mid = left + (right-left)/2;
+            if(target == nums[mid])
+            {
+                first = mid;
+                right = mid-1;
+            }
+            else if (target < nums[mid])
+                right = mid-1;
+            else
+                left = mid+1;
+        }
+        return first;
+    }
+
+    int lastInd(vector<int>&nums, int target)
+    {
+        int left=0, right = nums.size()-1;
+        int last = -1;
+        while(left <= right)
+        {
+            int mid = left + (right - left)/2;
+            if(target == nums[mid])
+            {
+                last = mid;
+                left = mid+1;
+            }
+            else if(target < nums[mid])
+                right = mid-1;
+            else
+                left = mid+1;
+        }
+        return last;
+    }
+
+vector<int> searchRange(vector<int>& nums, int target) 
+    {
+        int first = firstInd(nums, target);
+        if(first == -1) return {-1, -1};
+        int last = lastInd(nums, target);
+        return {first, last};
+    }
+};
+
+
+
+
+
+
+
+
+
+
+//discarded solution as if more than two duplicates there it fails 
+//though it passed 63/66 tests but not reliable not a solution
+/*
+    vector<int> searchRange(vector<int>& nums, int target) 
+    {
+        int n= nums.size();
+        int left=0, right=n-1;
+        while(left<=right)
+        {
+            int mid = left + (right-left)/2;
+            if(target == nums[mid])
+            {
+                
+                while(target == nums[])
+                    return {mid-1, mid};
+                else if(mid != n-1 && target == nums[mid+1])
+                    return {mid, mid+1};
+                else
+                    return {mid, mid};
+            }
+            else if(target < nums[mid])
+            {
+                right = mid-1;
+            }
+            else
+            {
+                left = mid+1;
+            }
+
+        }
+        return {-1, -1};
+    }
+};
+
+*/
