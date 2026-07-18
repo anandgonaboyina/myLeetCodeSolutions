bbc5c6dd71ed737eecf38cac44b61aed8ff7d794
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-18 12:28:54 +0530

    Add solution for Count Inversions

--- 0000-Strivers-A2Z-DSA-Sheet/Count-Inversions/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Count-Inversions/solution.cpp
@@ -1,22 +1,11 @@
-//similar follow up problem in leet code : 315. Count of Smaller Numbers After Self
-//my brute force solution
-class Solution {
-public:
-   long long int numberOfInversions(vector<int> nums) 
-    {
-        int n = nums.size();
-        
-    }
-};
 
-//from leetcode similar problem but asking total sum of array almost
-/*
+//similar follow up problem in leet code : 315. Count of Smaller Numbers After Self
 //my brute force solution passed 63/66 but still it can encounter the N^2 TLE
 // when array is completely has distinct numbers
 /*
 class Solution {
 public:
-    vector<int> countSmaller(vector<int>& nums) 
+    int numberOfInversions(vector<int>& nums) 
     {
         int n  = nums.size();
         if(n<2) return {0};
@@ -42,28 +31,76 @@ public:
                 p++;
                }
         }
-        return ans;
+        int sum=0;
+        for(int x : ans)
+            sum += x;
+        return sum;
     }
 };
 
 */
 
-// optimal solution  // need to complete this
+// optimal solution using merge sort algorithem logic
 class Solution {
 public:
-    vector<int> countSmaller(vector<int>& nums) 
+
+    void mergeSort(vector<pair<int, int>> &numsInd, int left, int right, vector<int>&ans)
+    {
+        if(left>=right) return;
+        int m = (left+right)/2;
+        mergeSort(numsInd, left, m, ans);
+        mergeSort(numsInd, m+1, right, ans);
+        merge(numsInd, left, m, right, ans);
+    }
+
+    void merge(vector<pair<int, int>> &numsInd, int left, int m, int right, vector<int>&ans)
+    {
+        int i=left, j=m+1;
+        int rightCnt=0;
+        vector<pair<int,int>> temp;
+        while(i<=m && j<=right)
+        {
+            if(numsInd[i].first <= numsInd[j].first)
+            {
+                temp.push_back(numsInd[i]);
+                ans[numsInd[i].second] += rightCnt;
+                i++;
+            }
+            else
+            {
+                temp.push_back(numsInd[j]);
+                rightCnt++;
+                j++;
+            }
+        }
+        while(i<=m)
+        {
+            temp.push_back(numsInd[i]);
+            ans[numsInd[i].second] +=rightCnt;
+            i++;
+        }
+        while(j<=right)
+        {
+            temp.push_back(numsInd[j]);
+            j++;
+        }
+        for(int i=left; i<=right; i++)
+            numsInd[i]=temp[i-left];
+    }
+
+    int numberOfInversions(vector<int>& nums) 
     {
         int n  = nums.size();
         if(n<2) return {0};
-        vector<int> ans;
-        int p=0;
-        while(p<n)
-        {
-            
-            p++;
-        }
-        return ans;
+        vector<int> ans(n, 0);
+        vector<pair<int, int>>numsInd(n);
+        for(int i=0; i<n; i++)
+            numsInd[i]={nums[i],i};
+
+        mergeSort(numsInd, 0, n-1, ans);
+        int sum=0;
+        for(int x : ans)
+            sum += x;
+        return sum;
     }
 };
-
-/*
