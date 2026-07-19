332c8b92bb570a4a634e8ebb39c2d2a75d9a189a
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-19 16:00:40 +0530

    Add solution for Find peak element

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Find-peak-element/solution.cpp
@@ -0,0 +1,20 @@
+//my brute force solution = optimal solution
+class Solution {
+public:
+    int pascalTriangleI(int r, int c) 
+    {
+        vector<vector<int>> ans;
+        for(int i=0; i<r; i++)
+        {
+            vector<int> row(i+1, 1);
+            ans.push_back(row);
+            for(int j=1; j<i; j++)
+            {
+                ans[i][j]=ans[i-1][j-1] + ans[i-1][j];
+                if(r==i+1 && c == j+1)
+                    return ans[i][j];
+            }
+        }
+        return -1;
+    }
+};
