51802ec10d7e1d6ee4a0d77759047d5dddc2bfe7
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-19 16:56:53 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.49%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,4 +1,5 @@
 //brute force solution
+/*
 class Solution {
 public:
     int mySqrt(int x) 
@@ -12,4 +13,27 @@ public:
     }
 };
 
+*/
 //optimal solution with binary search
+
+
+class Solution {
+public:
+    int mySqrt(int x)
+    {
+        int left=0, right=x;
+        int ans=0;
+        while(left<=right)
+            {
+              int  mid = left + (right-left)/2;
+                if((long long)mid*mid <= x)
+                    {
+                        left = mid+1;
+                        ans = mid;
+                    }
+                else
+                    right = mid-1;
+            }
+        return ans;
+    }
+};
