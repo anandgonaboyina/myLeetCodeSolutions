ea749299f7a40b5586e59e5197ea021eb4f1a97c
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-19 15:17:50 +0530

    Time: 0 ms (100%), Space: 26.3 MB (35.31%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,3 +1,29 @@
+// my  optimal solution
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) 
+    {
+        int left=0, right = nums.size()-1;
+            while(left<right)
+            {
+                int mid = left + (right-left)/2;
+                if(mid%2 == 1)
+                    mid--;
+                if(nums[mid]==nums[mid+1])
+                    left = mid +2;
+                else 
+                    right = mid;
+            }
+            return nums[left];
+    }
+};
+
+
+
+
+// not a binary search just one of the search for unsorted array
+
+/*
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) 
@@ -10,3 +36,4 @@ public:
         return xorN;
     }
 };
+*/
