2d93cccedee01525bef29b4f34a2aab694d875a3
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-19 16:21:49 +0530

    Time: 10 ms (13.14%), Space: 8.4 MB (87.52%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,15 @@
+//brute force solution
+class Solution {
+public:
+    int mySqrt(int x) 
+    {
+        int ans=0;
+        for(int i=1; (long long)i*i<=x; i++)
+        {
+            ans = i;
+        }
+        return ans;
+    }
+};
+
+//optimal solution with binary search
