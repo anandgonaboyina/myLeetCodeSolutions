a995881162ff7e9c1b53de897b1d43db00695e2f
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-20 12:43:53 +0530

    Add solution for Counting Frequencies of Array Elements

--- 0000-Strivers-A2Z-DSA-Sheet/Counting-Frequencies-of-Array-Elements/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Counting-Frequencies-of-Array-Elements/solution.cpp
@@ -3,11 +3,11 @@ public:
     vector<vector<int>> countFrequencies(vector<int>& nums) 
     {
         unordered_map<int, int> mp;
+        vector<vector<int>> vt;
         for(int i=0; i<nums.size(); i++)
         {
             mp[nums[i]]++;
         }
-        vector<vector<int>> vt;
         for(auto it : mp)
         {
             vt.push_back({it.first,it.second});
