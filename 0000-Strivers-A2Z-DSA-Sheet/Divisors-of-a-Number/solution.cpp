5607697d221c2b769f573dcb79bffbb6467cb8cb
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-20 10:43:43 +0530

    Add solution for Divisors of a Number

--- 0000-Strivers-A2Z-DSA-Sheet/Divisors-of-a-Number/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Divisors-of-a-Number/solution.cpp
@@ -1,3 +1,4 @@
+//brute force solution tc is N
 class Solution {
 public:
     vector<int> divisors(int n) {
@@ -6,6 +7,25 @@ public:
             {
                 if(n%i==0) vt.push_back(i);
             }
+        return vt;
+    }
+};
+// optimal solution to make tc to root(N)
+//since the smaller divisor in a pair will never exceed sqrt(n)
+class Solution {
+public:
+    vector<int> divisors(int n) {
+        vector<int>vt;
+        for(int i=1; i*i<=n; i++)
+            {
+                if(n%i==0)
+                {
+                    vt.push_back(i);
+                    if(i*i != n)
+                        vt.push_back(n/i);
+                }
+            }
+            sort(vt.begin(), vt.end());
         return vt;
     }
 };
