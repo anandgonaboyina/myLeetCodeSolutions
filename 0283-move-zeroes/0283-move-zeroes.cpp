b562476a519e72490be49412d8faa4b915b845d4
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-20 15:43:54 +0530

    Time: 0 ms (100%), Space: 23.8 MB (56.44%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -17,27 +17,3 @@ public:
 
 
 
-
-
-
-
-
-
-
-
-//class Solution {
-// public:
-//     void moveZeroes(vector<int>& nums) 
-//     {
-//         int k=0, n=nums.size();
-//         for(int i=0; i<n; i++)
-//         {
-//             if(nums[i]!=0)
-//             {
-//                 swap(nums[k],nums[i]);
-//                 k++;
-//             }
-//         }
-//     }
-// };
-
