799709c1fec27e6677d78fec8c26fa6fe978b898
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-20 10:43:55 +0530

    Add solution for Check for Prime Number

--- 0000-Strivers-A2Z-DSA-Sheet/Check-for-Prime-Number/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Check-for-Prime-Number/solution.cpp
@@ -1,7 +1,8 @@
+//optimal Solution from divisors method
+// as divisors of a number is never exceed square root and bigger will get from n/i;
 class Solution {
 public:
     bool isPrime(int n) {
-        //your code goes here
         if(n<=1) return false;
         for(int i=2; i*i<=n; i++)
             {
