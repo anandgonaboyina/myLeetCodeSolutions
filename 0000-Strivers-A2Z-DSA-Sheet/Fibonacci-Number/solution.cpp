312548bb2c61113c3b2b7d3c3b4dace32a0fbfd5
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-20 12:37:24 +0530

    Add solution for Fibonacci Number

--- 0000-Strivers-A2Z-DSA-Sheet/Fibonacci-Number/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Fibonacci-Number/solution.cpp
@@ -1,8 +1,10 @@
-class Solution {
+//solution
 public:
-    int fib(int n) {
-        //your code goes here
-        if(n<=1) return n;
-        return fib(n-1)+fib(n-2);
-    }
+    int fib(int n) 
+    }
 };
+
+    {
+        return fib(n-1)+fib(n-2);
+        if(n<=1) return n;
+class Solution {
