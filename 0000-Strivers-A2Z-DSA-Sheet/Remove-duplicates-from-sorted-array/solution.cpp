3c94d64ef056fb69a0bf61ba5523351e6672292d
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-20 15:10:12 +0530

    Add solution for Remove duplicates from sorted array

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Remove-duplicates-from-sorted-array/solution.cpp
@@ -0,0 +1,24 @@
+*/
+//without set
+/*
+public:
+    int removeDuplicates(vector<int>& nums)
+    {
+        int n=nums.size(), k=0;
+
+class Solution {
+        if(n<2) return n;
+};
+        }
+        return st.size();
+    }
+        for(int i=0; i<n; i++)
+        {
+            st.insert(nums[i]);
+        int n = nums.size();
+        set<int> st;
+public:
+    int removeDuplicates(vector<int>& nums) 
+    {
+/*
+class Solution {
