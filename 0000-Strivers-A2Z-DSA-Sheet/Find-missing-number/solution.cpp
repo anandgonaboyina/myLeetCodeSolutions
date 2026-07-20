6e56072c501b8cb13da923a24e8d2ed30f0a8264
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-20 16:09:53 +0530

    Add solution for Find missing number

--- 0000-Strivers-A2Z-DSA-Sheet/Find-missing-number/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Find-missing-number/solution.cpp
@@ -1,6 +1,14 @@
+//leetcode problem
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        //solved in leetCode
+       int xor1 =0, xor2=0;
+        int n = nums.size();
+       for(int i=0; i<n; i++)
+       {
+            xor1 ^=nums[i];
+            xor2 ^=(i+1);
+       }
+       return xor1 ^ xor2;
     }
 };
