b8de753ec37e73f3e4fbdded98e6519c41bfa76b
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-22 11:51:27 +0530

    Add solution for Count Occurrences in a Sorted Array

--- 0000-Strivers-A2Z-DSA-Sheet/Count-Occurrences-in-a-Sorted-Array/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Count-Occurrences-in-a-Sorted-Array/solution.cpp
@@ -8,13 +8,12 @@ public:
         while(left <= right)
         {
             int mid = left + (right-left)/2;
-            if(target == nums[mid])
+            if(target <= nums[mid])
             {
-                first = mid;
+                if(target==nums[mid])
+                    first = mid;
                 right = mid-1;
             }
-            else if (target < nums[mid])
-                right = mid-1;
             else
                 left = mid+1;
         }
@@ -28,15 +27,14 @@ public:
         while(left <= right)
         {
             int mid = left + (right - left)/2;
-            if(target == nums[mid])
+            if(target >= nums[mid])
             {
-                last = mid;
+                if(target==nums[mid])
+                    last = mid;
                 left = mid+1;
             }
-            else if(target < nums[mid])
-                right = mid-1;
             else
-                left = mid+1;
+                right = mid-1;
         }
         return last;
     }
