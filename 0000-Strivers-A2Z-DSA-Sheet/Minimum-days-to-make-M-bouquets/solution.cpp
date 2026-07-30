f57615185b9e692972287ec802a2a501e4fb146b
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-30 16:25:56 +0530

    Refactor solution for Minimum Days to Make M Bouquets
    Refactored the solution for the LeetCode problem 1482 to improve clarity and functionality. Removed unnecessary comments and implemented a binary search approach for calculating the minimum days to make M bouquets.

--- 0000-Strivers-A2Z-DSA-Sheet/Minimum-days-to-make-M-bouquets/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Minimum-days-to-make-M-bouquets/solution.cpp
@@ -1,52 +1,47 @@
-//leetcode problem : 704 binary search
-//my brute force solution is optimal solution
-//iterative method
-/*
+//leet code problem : 1482. Minimum Number of Days to Make m Bouquets
 class Solution {
 public:
-    int search(vector<int>& nums, int target)
-     {
-        int n = nums.size();
-        int left=0;
-        int right=n-1;
+
+    int calBouqets(vector<int>&bloomDay, int k, int days)
+    {
+        int cnt=0;
+        int bouqets=0;
+        for(int x : bloomDay)
+        {
+            if(days>=x)
+             {   cnt++;
+                 if(cnt == k)
+                    {
+                        bouqets++;
+                        cnt=0;
+                    }
+             }
+            else
+                cnt=0;
+        }
+        return bouqets;
+    }
+
+int roseGarden(int n, vector<int> bloomDay, int k, int m)
+    {
+        if((long long)k*m > n) return -1;
+        int left=1, right=0;
+        for(int x : bloomDay)
+            right = max(right, x);
+        int ans = right;
         while(left<=right)
         {
-            int mid = left + (right-left)/2;   
-            // (left+right)/2; may give integer overflow for 32 bit if array size if large 
-            // before divison happen
-            if(nums[mid]==target)
-                return mid;
-            else if(nums[mid]<target)
-                right=mid-1
+            int mid = left + (right-left)/2;
+            int bouqets = calBouqets(bloomDay, k, mid);
+            if(bouqets >= m)
+                {
+                    ans = mid;
+                    right=mid-1;
+                }
             else
-                left=mid+1;
+                left = mid+1;
         }
-        return -1;
-    }
-};
-
-*/
-
-//recursive method
-class Solution {
-public:
-    int bSearch(vector<int>&nums, int left, int right, int target)
-    {
-        if(left>right) return -1;
-        int mid = left + (right-left)/2;
-        if(target==nums[mid])
-            return mid;
-        else if(target<nums[mid])
-            return bSearch(nums, left, mid-1, target);
-        else
-            return bSearch(nums, mid+1, right, target);
-    }
-
-    int search(vector<int>& nums, int target)
-     {
-        int n = nums.size();
-            int res = bSearch(nums, 0, n-1, target);
-        return res;
+        return ans;
     } 
 };
 
