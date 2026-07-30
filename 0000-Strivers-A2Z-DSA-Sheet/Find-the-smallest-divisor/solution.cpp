8151f4330f7c340f04da889e44ab921d5c2488a6
anandgonaboyina <anandgonaboyina@gmail.com>
2026-07-30 16:57:42 +0530

    Add solution for Find the smallest divisor

--- 0000-Strivers-A2Z-DSA-Sheet/Find-the-smallest-divisor/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Find-the-smallest-divisor/solution.cpp
@@ -1,27 +1,36 @@
-//similar problem leetcode problem : 35 Search Insert Position
-//just modification of it means dont return index of target if found that sit;
-class Solution{
+// leet code problem : 1283. Find the Smallest Divisor Given a Threshold
+//optimal solution again adopted form previous koko eating bananas problem pattern
+class Solution {
 public:
-    int upperBound(vector<int> &nums, int x)
+    int calDivSum(vector<int>&nums, int div)
     {
-        int n = nums.size();
-        int left=0, right=n-1;
-        int ans=n;   // if no num found size of array
-        while(left<=right)
+        int sum=0;
+        for(int x : nums)
+        {
+            sum += x/div;
+            if(x%div !=0)
+                sum++;
+        }
+        return sum;
+    }
+    int smallestDivisor(vector<int>& nums, int threshold) 
+    {
+        int left=1, right=0;
+        for(int x : nums)
+            right = max(right, x);
+        int ans = right;
+        while(left <= right)
         {
             int mid = left + (right-left)/2;
-            if(x < nums[mid])
-            {
-                ans = mid;
-                right = mid-1;
-            }
+            int sum = calDivSum(nums, mid);
+            if(sum<= threshold)
+                {
+                    ans = mid;
+                    right = mid-1;
+                }
             else
-            {
                 left = mid+1;
-            }
         }
         return ans;
     }
 };
-
-
