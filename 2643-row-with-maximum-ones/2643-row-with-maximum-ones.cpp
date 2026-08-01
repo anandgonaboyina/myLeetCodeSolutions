8742ab7f15079f57d82e26566074cc89a3429325
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-01 12:57:56 +0530

    Time: 2 ms (65.58%), Space: 64.7 MB (70.6%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.cpp
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.cpp
@@ -1,4 +1,6 @@
-//my brute force solution not binary solution
+//my brute force solution not binary solution as they didnt metnions it is sorted rows
+// this is the optimal brute force solution
+
 class Solution {
 public:
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) 
@@ -19,3 +21,38 @@ public:
         return ans;
     }
 };
+
+
+// optimal binary solution  for sorted rows fails for unsorted rows here it failed at 179/1049 tests passed
+/*
+class Solution {
+  public:   
+  vector<int> rowAndMaximumOnes(vector <vector< int >> & mat) 
+  {
+    int n = mat[0].size();
+    vector<int> ans(2, 0); 
+    for(int i=0; i<mat.size(); i++)
+    {
+        if(mat[i][n-1]==0)
+            continue;
+        int left=0, right=n-1;
+            while(left <=right)
+            {
+                int mid = left + (right-left)/2;
+                if(mat[i][mid]==1)
+                {
+                    right = mid-1;
+                    if(ans[1] < n-mid)
+                    {
+                        ans = {i, n-mid};
+                    }
+                }
+                else
+                    left = mid+1;
+            }
+    }
+    return ans;
+  }
+};
+
+*/
