a8943e07f73b7612b6e0295c576395dfd3f3ddd8
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-03 20:23:47 +0530

    Add solution for Find Peak Element - II

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Find-Peak-Element-II/solution.cpp
@@ -0,0 +1,23 @@
+
+*/
+            
+    }
+};
+            }
+                return {-1, -1};
+                    };
+                if( (i==m-1 || mat[i][peak]>mat[i+1][peak] ) && (i==0 || mat[i][peak] > mat[i-1][peak]))
+                    return {i, peak};
+                for(int j=0; j<n; j++)
+                    {
+                        peak = mat[i][peak]>mat[i][j]? peak: j;
+            {
+                int peak = 0;
+        int m = mat.size();
+        int n=mat[0].size();
+        for(int i=0; i<m; i++)
+public:
+    vector<int> findPeakGrid(vector<vector<int>>& mat) 
+    {
+/*
+class Solution {
