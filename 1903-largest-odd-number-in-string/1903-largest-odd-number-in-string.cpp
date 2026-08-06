c00e37aeed2f33afd671bcd0e6f3bb7c32de3787
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-06 19:56:12 +0530

    Time: 0 ms (100%), Space: 17.8 MB (39.56%) - LeetHub

---
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -0,0 +1,33 @@
+// optimal solution
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int right = 0;
+        for(int i=num.length()-1; i>=0; i--)
+        {
+            if((num[i]-0) %2 != 0)
+            {
+                return num.substr(0, i+1);
+            }
+        }
+        return "";
+    }
+};
+
+
+/*
+// foolish solution as just need to care about the last digit if it odd we can return the string upto there
+//that too the length of the string is till 10^5 means greater 10 digits means out of int overflow
+        int n = 0;
+        for(char x : num)
+        {
+                n += (int)x - 48;
+        }
+        while(n% 2 == 0)
+            n = n/10;
+        string ans = "";
+        if(n !=0)
+            ans += n;
+        return ans;
+
+*/
