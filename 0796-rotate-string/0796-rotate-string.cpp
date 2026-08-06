49ac4f816f2e56e6b1d6d8e6f2b23ee1f5cacc37
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-06 23:05:14 +0530

    Time: 0 ms (100%), Space: 8 MB (65.51%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,5 +1,7 @@
 //my brute force solution adopted from the logic of array rotate
+// got 100% beats
 
+/*
 class Solution {
 public:
     bool rotateString(string s, string goal) {
@@ -13,6 +15,21 @@ public:
             if(s2==goal)
                 return true;
         }
+        return false;
+    }
+};
+*/
+//optimal way to solve this problem
+//better logic for less code
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length() != goal.length())
+            return false;
+        string doubleStr = s + s;
+        if(doubleStr.find(goal) != -1)
+            return true;
         return false;
     }
 };
