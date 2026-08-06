fbbe20425817089ea3597a64f74580ac5da7853d
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-06 20:49:32 +0530

    Time: 11 ms (3.78%), Space: 18.2 MB (5.18%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,3 +1,28 @@
+//my brute force solution
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int right=0;
+        for(int i=0; i<strs[0].size(); i++)
+        {
+            for(string s : strs)
+            {
+                if(s[i] != strs[0][i])
+                    return strs[0].substr(0, i);
+            }
+                right++;
+        }
+        return strs[0].substr(0, right);
+    }
+};
+
+
+
+
+
+
+// old solution 
+/*
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
@@ -14,4 +39,6 @@ public:
         return st;
     }
 };
-//anand kumar
+
+
+*/
