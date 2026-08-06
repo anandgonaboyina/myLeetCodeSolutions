3a0156508800d5e10d588fd259eaf43f905b3412
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-06 23:07:22 +0530

    Add solution for Rotate String

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Rotate-String/solution.cpp
@@ -0,0 +1,22 @@
+*/
+//optimal way to solve this problem
+    }
+};
+                return true;
+        }
+        return false;
+            reverse(s2.begin(), s2.begin()+i+1);
+            reverse(s2.begin()+i+1, s2.end());
+            if(s2==goal)
+            string s2 = s;
+            reverse(s2.begin(), s2.end());
+        
+        for(int i=0; i<s.size(); i++)
+        {
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+/*
+//my brute force solution adopted from the logic of array rotate
+// got 100% beats
+//better logic for less code
