615e1ed7c932f3dcc0cfc9c60f8fb2e0e164414f
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-08 12:57:15 +0530

    Time: 0 ms (100%), Space: 10.8 MB (80.54%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,7 +1,8 @@
+//my brute solution 
 class Solution {
 public:
     string frequencySort(string s) {
-        // vector<int> hashArr(26, 0);
+        // vector<int> hashArr(26, 0);  //wont work
         vector<pair<int, char>> hashArr(123, {0, 0});
         string ans="";
         for(char ch : s)
@@ -9,7 +10,7 @@ public:
             hashArr[ch] = {++hashArr[ch].first, ch};
         }
         sort(hashArr.begin(), hashArr.end());
-        for(int i = 122; i>=0; i--)
+        for(int i = 122; i>=48; i--)    // chars and digits are from 48 and 65 to 122
         {
             if(hashArr[i].first !=0)
                 for(int j=0; j<hashArr[i].first; j++)
