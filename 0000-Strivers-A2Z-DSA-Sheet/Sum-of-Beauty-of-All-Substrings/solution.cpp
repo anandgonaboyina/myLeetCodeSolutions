58d5837c3e6bdca09e06f1562335ebd1e57fd8fd
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-09 13:36:27 +0530

    Add solution for Sum of Beauty of All Substrings

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Sum-of-Beauty-of-All-Substrings/solution.cpp
@@ -0,0 +1,30 @@
+//my brute force solution
+//opitmal solution note here we did inner loop too but it constant 26 so not comes to count of N powers okay
+
+class Solution {
+public:
+    // int calBeauty(string s, int left, int right)
+    int beautySum(string s) {
+        int n = s.size();
+        int maxFreq = 0;
+        int beauty = 0;
+        for(int i=0; i<n; i++)
+        {
+            vector<int> hashArr(26, 0);  // since only lowercase english letters
+            maxFreq = 0;
+            for(int j=i; j<n; j++)
+            {
+                hashArr[s[j]-'a']++;
+                maxFreq = max(maxFreq, hashArr[s[j]-'a']);
+                int minFreq=n+1;
+                for(int k=0; k<26; k++)
+                {
+                    if(hashArr[k] > 0 )    // the chars that not come  yet so they have 0 as value need to skip them
+                        minFreq = min(minFreq, hashArr[k]);
+                }
+            beauty += maxFreq - minFreq;
+            }
+        }
+        return beauty;
+    }
+};
