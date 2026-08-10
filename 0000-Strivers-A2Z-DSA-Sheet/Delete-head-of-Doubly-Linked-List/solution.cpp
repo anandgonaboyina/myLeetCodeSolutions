2f36282b110eeb63f53088122555185e4a2d2657
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-10 19:53:02 +0530

    Add solution for Delete head of Doubly Linked List

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Delete-head-of-Doubly-Linked-List/solution.cpp
@@ -0,0 +1,16 @@
+};
+*/
+
+class Solution {
+public:
+ *      ListNode(int x) : data(x), prev(nullptr), next(nullptr) {}
+ *      ListNode(int x, ListNode *prev, ListNode *next) : data(x), prev(prev), next(next) {}
+ *      ListNode *next;
+ *      ListNode() : data(0), prev(nullptr), next(nullptr) {}
+ *  public:
+ *      int data;
+ *      ListNode *prev;
+{
+ * Definition for doubly-linked list.
+/**
+class ListNode
