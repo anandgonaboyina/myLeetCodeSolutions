df4742647bb330050fdb6fde9a7b22b398c7b03a
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-10 20:40:51 +0530

    Add solution for Reverse a Doubly Linked List

--- 0000-Strivers-A2Z-DSA-Sheet/Reverse-a-Doubly-Linked-List/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Reverse-a-Doubly-Linked-List/solution.cpp
@@ -8,7 +8,22 @@ public:
     ListNode(int val) : data(val), prev(nullptr), next(nullptr) {}
 };
 */
-//my brute force solution TC is N and SC is 1
+
+
+/*
+## Note
+ Pointer Comparison
+we cannot use < or > to compare node pointers in a linked list. Linked list nodes are dynamically allocated in the heap, meaning they are scattered randomly in memory. right could actually have a lower memory address than left, causing your loop to run forever or access invalid memory.
+####################
+To stop the loop when pointers cross in a DLL, you must check for exact matches: 
+while (left != right && left->prev != right).
+*/
+//my brute force solution TC is N and SC is 1  but its not allowed a way to do
+//The Interview Trap:
+/*
+Swapping data inside nodes is generally considered a red flag in interviews. In real-world systems, the data payload of a node might be a massive object. Swapping heavy objects is incredibly slow. Instead, you are expected to leave the data alone and swap the pointers.
+*/
+/*
 class Solution {
 public:
     ListNode* reverseDLL(ListNode* head) {
@@ -19,7 +34,7 @@ public:
         {
             right = right->next;
         }
-        while(left < right)
+        while(left != right && left->prev != right)  // here not like left < right 
         {
             int temp = left->data;
             left->data = right->data;
@@ -28,5 +43,55 @@ public:
             right = right->prev;
         }
         return head;
+    }
+};
+*/
+//swaping the pointers my apporach
+/*
+class Solution{
+    public:
+    ListNode* reverseDLL(ListNode* head)
+    {
+        if(!head || !head->prev) return head;
+        ListNode* left =head;
+        ListNode* right =head;
+        while(right->next)
+        {
+            right = right->next;
+        }
+        head = right;
+        while(left != right && left->prev != right)
+        {
+            ListNode* tempL = left;
+            ListNode* tempR = right;
+            left = right;
+            right = tempL;
+            left->prev = tempL->prev;
+            left->next = tempL->next;
+            right->prev = tempR->prev;
+            right->next = tempR->next;
+        }
+        return head;
+    }
+};
+*/
+// better approach to swap the prev and next pointer and pointing to the second last prev node give the tail of the linkedlist which is the head to return
+//TC is O(N) with SC O(1).
+class Solution{
+    public:
+    ListNode* reverseDLL(ListNode* head)
+    {
+        if(!head || !head->next) return head;
+        ListNode* curr = head;
+        ListNode* temp = nullptr;
+        while(curr != nullptr)
+        {
+            temp = curr->prev;
+            curr->prev = curr->next;
+            curr->next = temp;
+
+            curr = curr->prev;
+        }
+        return temp->prev;
     }
 };
