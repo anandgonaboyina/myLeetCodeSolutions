321cf9ce25a8e2716b59261283b526eaf1fc4b71
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-10 19:44:44 +0530

    Add solution for Insert node before head in Doubly Linked List

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Insert-node-before-head-in-Doubly-Linked-List/solution.cpp
@@ -0,0 +1,24 @@
+/**
+class ListNode
+{
+ * Definition for doubly-linked list.
+ *  public:
+ *      int data;
+ *      ListNode *prev;
+ *      ListNode *next;
+ *      ListNode() : data(0), prev(nullptr), next(nullptr) {}
+ *      ListNode(int x) : data(x), prev(nullptr), next(nullptr) {}
+ *      ListNode(int x, ListNode *prev, ListNode *next) : data(x), prev(prev), next(next) {}
+};
+*/
+
+class Solution {
+public:
+    ListNode* insertBeforeHead(ListNode* head, int X) {
+        ListNode* temp = new ListNode(X, nullptr, head);
+        if(head != nullptr)
+            head->prev = temp;
+        head = temp;
+        return head;
+    }
+};
