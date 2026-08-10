cbd08e94931ca616ac3f87994a2188fe6ff6d9b7
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-10 19:24:48 +0530

    Add solution for Search in Linked List

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Search-in-Linked-List/solution.cpp
@@ -0,0 +1,19 @@
+        else if(head->val == key) return true;
+    bool searchKey(ListNode* head, int key) {
+        if(head->nullptr) return false;
+// recursive way with Sc as 1 and Tc as N
+class Solution {
+public:
+};
+*/
+        delete next;
+    }
+    ListNode* next;
+
+    ListNode(int value) : val(value), next(nullptr) {}
+
+    ~ListNode() {
+class ListNode {
+public:
+    int val;
+/*Defination of ListNode
