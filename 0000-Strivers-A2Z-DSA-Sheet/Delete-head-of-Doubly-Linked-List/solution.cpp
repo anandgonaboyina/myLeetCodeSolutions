0c1da4de2518b973d7f6fa5949e7a8b5724de873
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-10 19:53:41 +0530

    Add solution for Delete head of Doubly Linked List

--- 0000-Strivers-A2Z-DSA-Sheet/Delete-head-of-Doubly-Linked-List/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Delete-head-of-Doubly-Linked-List/solution.cpp
@@ -1,16 +1,45 @@
+/*
+Definition of singly linked list:
+class ListNode{
+  
+  public:
+    int data;
+    ListNode *next;
+    ListNode() : data(0), next(nullptr) {}
+    ListNode(int x) : data(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : data(x), next(next) {}
 };
 */
-
-class Solution {
+class Solution {
 public:
- *      ListNode(int x) : data(x), prev(nullptr), next(nullptr) {}
- *      ListNode(int x, ListNode *prev, ListNode *next) : data(x), prev(prev), next(next) {}
- *      ListNode *next;
- *      ListNode() : data(0), prev(nullptr), next(nullptr) {}
- *  public:
- *      int data;
- *      ListNode *prev;
-{
- * Definition for doubly-linked list.
-/**
-class ListNode
+    ListNode* deleteHead(ListNode* &head) {
+        if(head == nullptr)
+            return nullptr;
+        ListNode* temp = head;
+        head = head->next;
+        delete temp;
+        return head;
+    }
+};
+
+/*
+doubley linkedlist with prev and next as node pointers
+class Solution {
+public:
+    ListNode* deleteHead(ListNode* &head) {
+        if(head == nullptr)
+            return nullptr;
+        ListNode* temp = head;
+        head = head->next;
+
+// # here we have to check if only one node there head is pointing to nullptr
+// # so head->prev is going to give segmentation error
+
+        if(head != nullptr)           
+        head->prev = nullptr;
+        delete temp;
+        return head;
+    }
+};
+
+*/
