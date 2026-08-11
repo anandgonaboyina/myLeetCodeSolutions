43ba4989ef475fb3861fab21535ada7496806de9
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-11 19:37:03 +0530

    Add solution for Detect a loop in LL

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Detect-a-loop-in-LL/solution.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+//my brute force solution TC is N and SC is N so getting 8% beats
+/*
+class Solution{
+    public:
+    bool hasCycle(ListNode* head)
+    {
+        if(!head || !head->next) return false;
+        unordered_set<ListNode*> pointers;
+        ListNode* ptr= head;
+        while(ptr)
+        {
+            if(pointers.contains(ptr))
+                return true;
+            pointers.insert(ptr);
+            ptr = ptr->next;
+        }
+        return false;
+    }
+};
+*/
+
+ // optimal solution obtained from logic of Tortoise and Hare pattern
+ // got 81% beats TC is N and SC is 1
+ 
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head || !head->next)
+            return false;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next)
+           {
+                slow = slow->next;
+                fast = fast->next->next;
+                if(slow  == fast)
+                       return true;
+           }
+
+        return false;
+    }
+};
+
+
+ //my brute force 2 discarded approach because it wont work as it goes to inifinite loop if loop there
+// waste of time 
+/*
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head || !head->next)
+            return false;
+        ListNode* ptr1 = head;
+        ListNode* ptr2 = nullptr;
+        while(ptr1)
+           {
+                ptr2 = ptr1;
+                while(ptr2)
+                {
+                    if(ptr2 == ptr1)
+                        return true;
+                    ptr2 = ptr2->next;
+                }
+            ptr1 = ptr1->next;
+           }
+        return false;
+    }
+};
+*/
