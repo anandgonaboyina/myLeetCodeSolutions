8df0e63331559a9e0cb3e2cf09ecc0d062e4c483
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-12 18:43:27 +0530

    Add solution for Segregate odd and even nodes in Linked List

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Segregate-odd-and-even-nodes-in-Linked-List/solution.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+//leetcode problem : 328. Odd Even Linked List 
+ //optimal solution beats 100%  TC is N and SC is 1
+ //working logic is linking the odd and even nodes and attaching the first evenhead node to last odd->next
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;
+        while(even && even->next)
+        {
+            odd->next = odd->next->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
