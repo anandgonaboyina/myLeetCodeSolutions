6b72a521ba83c5b60811031dca6e68bc40702aa3
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-12 20:30:53 +0530

    Add solution for Remove Nth node from the back of the LL

--- 0000-Strivers-A2Z-DSA-Sheet/Remove-Nth-node-from-the-back-of-the-LL/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Remove-Nth-node-from-the-back-of-the-LL/solution.cpp
@@ -1,77 +1,55 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
- //my brute force TC is N and Sc is 1 got 100% beats though its done in two passes 
- /*
-class Solution {
-public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(!head) return head;
-        if(head && !head->next && n==1) return nullptr;
-        int len =0, fn = 0;
-        ListNode* ptr = head;
-        while(ptr)
-        {
-            ptr = ptr->next;
-            len++;
-        }
-        fn = len-n;  // node before that we delete
-        if(fn == 0)
-            {
-                ListNode* temp = head;
-                head=head->next;
-                delete temp;
-                return head;
-            }
-        ptr = head;
-        for(int i=1; i<fn; i++)
-        {
-            ptr = ptr->next;
-        }
-        ListNode* temp = ptr->next;
-        ptr->next = temp->next;
-        delete temp;
-        return head;
-    }
-};
-*/
-//optimal solution of above logic in single pass
-class Solution {
-public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* dummy  = new ListNode(0, head);
-        ListNode* slow = dummy;
-        ListNode* fast = dummy;
-        int cnt=1;
-        while(fast && cnt <=n)
-        {
-            fast = fast->next;
-            cnt++;
-        }
-        while(fast->next)
-        {
-            slow =slow->next;
-            fast = fast->next;
-        }
-        ListNode* temp = slow->next;
-        slow->next = temp->next;          // if head is need to delete then slow = dummy so dummy->next = head->next fine it saves us
-        delete temp;
-        head = dummy->next;
-        delete dummy;                       
-        return head;
-    }
-};
-//## Notes logic behind
+//brute force solution
 /*
- Creating a dummy node that points to head, and start both fast and slow at this dummy. This completely protects you from the edge case where we have to delete the very first node of the list.
-ny Using two pointers, fast and slow. Move fast forward exactly N steps. Now there is a gap of $N$ nodes between slow and fast.Move both pointers forward one step at a time until fast reaches the very last node.Because of the exact N-nodes gap, slow will now be pointing exactly at the node right before the one you need to delete.Re-wire the pointer: slow->next = slow->next->next;
-
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) 
+    {
+        vector<int> Pnums, Nnums;
+        int n = nums.size();
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]>0)
+            {
+                Pnums.push_back(nums[i]);
+            }
+            else
+            {
+                 Nnums.push_back(nums[i]);
+            }
+        }
+        int p=0,q=0;
+        for(int i=0; i<n; i++)
+        {
+                nums[i]=Pnums[p++];
+                i++;
+                nums[i]=Nnums[q++];
+        }
+        return nums;
+    }
+};
 */
+
+// Optimal Approach to make it in single pass
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) 
+    {
+        int n = nums.size();
+        vector<int> ans(n);
+        int posInd=0, negInd=1;
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i]>0)
+            {
+                ans[posInd]=nums[i];
+                posInd +=2;
+            }
+            else
+            {
+                 ans[negInd] = nums[i];
+                 negInd +=2;
+            }
+        }
+        return ans;
+    }
+};
