34e5effc56b86b575cb91069377dd21bb59d2296
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-17 19:47:14 +0530

    Add solution for Sort LL

--- 0000-Strivers-A2Z-DSA-Sheet/Sort-LL/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Sort-LL/solution.cpp
@@ -8,52 +8,50 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
- //optimal merge sort solution for linkedlist
+ //my brute force solution got 100% beats TC is N and SC is 1
+ /*
 class Solution {
 public:
-    ListNode* sortLL(ListNode* head)
-    {
+    ListNode* middleOfLinkedList(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* ptr = head;
+        int cnt =0;
+        while(ptr)
+        {
+            cnt++;
+            ptr = ptr->next;
+        }
+        ptr = head;
+        for(int i=0; i<cnt/2; i++)
+        {
+            ptr = ptr->next;
+        }
+        return ptr;
+    }
+};
+*/
+/*
+an interview, the immediate follow-up question will be: "Can you find the middle in exactly one pass instead of one and a half?"
+To do this, you use the Tortoise and Hare (Slow and Fast Pointers) technique.
+## The Hint:  ##
+Create two pointers: slow and fast. Start both at head.
+Run a while loop.
+Move fast forward by two steps (fast = fast->next->next).
+Move slow forward by one step (slow = slow->next).
+Because the fast pointer travels exactly twice as fast, by the time it hits the end of the list (nullptr), the slow pointer will be trapped exactly in the middle.
+*/
+// TC is N in one pass and SC is 1
+class Solution {
+public:
+    ListNode* middleOfLinkedList(ListNode* head) {
         if(!head || !head->next) return head;
         ListNode* slow = head;
-        ListNode* fast = head->next;
+        ListNode* fast = head;
         while(fast && fast->next)
         {
             slow = slow->next;
-            fast = fast->next->next; 
+            fast = fast->next->next;
         }
-        ListNode* rightHalf = slow->next;
-        slow->next = nullptr;
-        ListNode* leftHead = sortLL(head);
-        ListNode* rightHead = sortLL(rightHalf);
-        return mergeLL(leftHead, rightHead);
-    }
-    ListNode* mergeLL(ListNode* l1, ListNode* l2)
-    {
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
-        while(l1 && l2)
-        {    if(l1->val <= l2->val)
-            {
-                
-                curr->next = l1;
-                l1 = l1->next;
-            }
-            else
-            {
-            curr->next = l2;
-            l2 = l2->next;
-            }
-            curr = curr->next;
-        }
-        if(l1) curr->next = l1;
-        else
-            curr->next =l2;
-        ListNode* newHead = dummy->next;
-        delete  dummy;
-        return newHead;
-    }
-    ListNode* sortList(ListNode* head) {
-        return sortLL(head);
+        return slow;
     }
 };
