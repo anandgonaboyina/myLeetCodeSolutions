a2ed27ed6a601caa43073217e5bbfa0ec19daae9
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-19 16:45:43 +0530

    Time: 0 ms (100%), Space: 13.6 MB (8.41%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,7 +11,7 @@
  //my brute force solution got 100% beats after spending 1hr
  // this is the optimal solution TC is N and SC is 1
  //iterative solution
-
+/*
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
@@ -29,19 +29,51 @@ public:
     }
 };
 
-
 // recursive solution
+//TC is N and SC is N due to recursion stack goes up to n levels deep
 /*
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         if(!head || !head->next) return head;
-            ListNode* newHead = reverseList(head->next);
-            head->next->next = head;
-            head->next =nullptr;
+            ListNode* newHead = reverseList(head->next);                              // this is going to carry back til the first node of list without changing
+            head->next->next = head;                                                   // linking the next node's next back to present node
+            head->next =nullptr;                                                       // present node to the nullptr think it as a LL with 2 nodes
         return newHead;
     }
 };
-
 */
 
+/*
+
+//revision cnt 1
+// iterative
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* curr = head;
+        ListNode* prev = nullptr;
+        while(curr)
+        {
+            ListNode* temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+};
+*/
+//recursive
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode* nextNode = head->next;
+        ListNode* newHead = reverseList(head->next);
+        nextNode->next = head;
+        head->next =nullptr;
+        return newHead;
+    }
+};
