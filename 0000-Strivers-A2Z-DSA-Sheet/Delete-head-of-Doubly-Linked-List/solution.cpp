de90e8db024d45b19bd35e666565f4ddea07aef7
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-19 15:18:37 +0530

    Add solution for Delete head of Doubly Linked List

--- 0000-Strivers-A2Z-DSA-Sheet/Delete-head-of-Doubly-Linked-List/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Delete-head-of-Doubly-Linked-List/solution.cpp
@@ -1,61 +1,38 @@
-/*
-Definition of singly linked list:
-class ListNode{
-  
-  public:
-    int data;
-    ListNode *next;
-    ListNode() : data(0), next(nullptr) {}
-    ListNode(int x) : data(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : data(x), next(next) {}
+/**
+class ListNode
+{
+ * Definition for doubly-linked list.
+ *  public:
+ *      int data;
+ *      ListNode *prev;
+ *      ListNode *next;
+ *      ListNode() : data(0), prev(nullptr), next(nullptr) {}
+ *      ListNode(int x) : data(x), prev(nullptr), next(nullptr) {}
+ *      ListNode(int x, ListNode *prev, ListNode *next) : data(x), prev(prev), next(next) {}
 };
 */
+
 class Solution {
 public:
-    ListNode* deleteHead(ListNode* &head) {
-        if(head == nullptr)
-            return nullptr;
-        ListNode* temp = head;
-        head = head->next;
-        delete temp;
+    ListNode* insertBeforeHead(ListNode* head, int X) {
+        ListNode* temp = new ListNode(X, nullptr, head);
+        if(head != nullptr)
+            head->prev = temp;
+        head = temp;
         return head;
     }
 };
 
-/*
-doubley linkedlist with prev and next as node pointers
-class Solution {
-public:
-    ListNode* deleteHead(ListNode* &head) {
-        if(head == nullptr)
-            return nullptr;
-        ListNode* temp = head;
-        head = head->next;
-
-// # here we have to check if only one node there head is pointing to nullptr
-// # so head->prev is going to give segmentation error
-
-        if(head != nullptr)           
-        head->prev = nullptr;
-        delete temp;
-        return head;
-    }
+//revision 1
+class Solution
+{
+    public:
+        ListNode* insertBeforeHead(ListNode* head, int X)
+        {
+            ListNode* newHead = new ListNode(X, nullptr, head);
+            if(head)
+                head->prev = newHead;
+            head = newHead;
+            return head;
+        }
 };
-*/
-
-// clean code 
-/*
-
-class Solution {
-public:
-    ListNode *deleteHead(ListNode *&head) {
-        if(!head) return nullptr;
-        ListNode* temp = head;
-        head = head->next;
-        if(head)
-            head->prev = nullptr;
-        delete temp;
-        return head;
-    }
-};
-*/
