e2894add110e8300d11943b131b10adf40c0f02f
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-19 18:50:31 +0530

    Time: 86 ms (9.15%), Space: 33.5 MB (5.64%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -7,6 +7,8 @@
  * };
  */
  //my brute force solution
+ //got 5% beats
+ /*
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
@@ -26,5 +28,31 @@ public:
         return nullptr;
     }
 };
-
-//optimal solution
+*/
+//better solution
+//TC is M+N and SC is M+N;
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* ptr1 = headA;
+        ListNode* ptr2 = headB;
+        unordered_set<ListNode*> hashArr;
+        while(ptr1 || ptr2)
+        {
+            if(hashArr.contains(ptr1)) return ptr1;
+            if(ptr1) 
+            {
+                hashArr.insert(ptr1);
+                ptr1= ptr1->next;
+            }
+            if(hashArr.contains(ptr2))
+                return ptr2;
+           if(ptr2)
+           {
+                hashArr.insert(ptr2);
+                ptr2 = ptr2->next;
+           }
+        }
+        return nullptr;
+    }
+};
