348e5e42b0d5b16e8ec5390fbbd2682d4838a4ae
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-19 19:35:48 +0530

    Add solution for Add one to a number represented by LL

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Add-one-to-a-number-represented-by-LL/solution.cpp
@@ -0,0 +1,21 @@
+        while(ptr->next)
+        {
+            ptr = ptr->next;
+        }
+        if(ptr->val != 9)
+        ListNode* prev = head;
+            prev = ptr;
+        ListNode* ptr = head;
+public:
+    ListNode *addOne(ListNode *head) {
+        if(!head) return head;
+//my brute force solution
+class Solution {
+*/
+//LeetCode 369: Plus One Linked List => Premium problem locked
+    }
+};
+        val = data1;
+        next = next1;
+    ListNode(int data1, ListNode *next1)
+    {
