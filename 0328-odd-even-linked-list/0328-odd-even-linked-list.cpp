824248cf79b28b79ce5d4f49315b518350b4fcd7
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-19 17:59:13 +0530

    Time: 0 ms (100%), Space: 15.5 MB (64.04%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -9,8 +9,9 @@
  * };
  */
 
- //optimal solution beats 100%  TC is N and SC is 1
-  //working logic is linking the odd and even nodes separately and attaching the first evenhead node to last odd->next of the odd linked nodes
+//optimal solution beats 100%  TC is N and SC is 1
+//working logic is linking the odd and even nodes separately and attaching the first evenhead node to last odd->next of the odd linked nodes
+/*
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
@@ -27,5 +28,38 @@ public:
         }
         odd->next = evenHead;
         return head;
+    }
+};
+*/
+//revision 1
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head || !head->next) return head;
+        ListNode dummyEven(0);
+        ListNode dummyOdd(0);
+        ListNode* ptr = head;
+        ListNode* optr = &dummyOdd;
+        ListNode* eptr = &dummyEven;
+        int n =1;
+        while(ptr)
+        {
+            if(n%2==1)
+                {
+                    optr->next = ptr;
+                    optr = optr->next;
+                }
+            else
+                {
+                    eptr->next = ptr;
+                    eptr = eptr->next;
+                }
+            ptr = ptr->next;
+            n++;
+        }
+        optr->next = dummyEven.next;
+        eptr->next = nullptr;
+        return dummyOdd.next;
     }
 };
