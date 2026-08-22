562977df7b0c33835e40c71e3b534240bd67a86d
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-22 07:30:56 +0530

    Time: 5 ms (97.24%), Space: 57.1 MB (74.24%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -12,66 +12,67 @@
  //optimal merge sort solution for linkedlist
  //getting 5% beats only all the time if we use heap initilization of the dummy node
  //getting 80% betas if we use stack dummy variable of node
-class Solution {
-public:
-    ListNode* sortLL(ListNode* head)
-    {
-        if(!head || !head->next) return head;
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        while(fast && fast->next)
-        {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        ListNode* rightHalf = slow->next;
-        slow->next = nullptr;
-        ListNode* leftHead = sortLL(head);
-        ListNode* rightHead = sortLL(rightHalf);
-        return mergeLL(leftHead, rightHead);
-    }
-    ListNode* mergeLL(ListNode* l1, ListNode* l2)
-    {
-        //below is heap method takes time for memory allocation and delete
-        /*
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
-        */
+ 
+// class Solution {
+// public:
+//     ListNode* sortLL(ListNode* head)
+//     {
+//         if(!head || !head->next) return head;
+//         ListNode* slow = head;
+//         ListNode* fast = head->next;
+//         while(fast && fast->next)
+//         {
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+//         ListNode* rightHalf = slow->next;
+//         slow->next = nullptr;
+//         ListNode* leftHead = sortLL(head);
+//         ListNode* rightHead = sortLL(rightHalf);
+//         return mergeLL(leftHead, rightHead);
+//     }
+//     ListNode* mergeLL(ListNode* l1, ListNode* l2)
+//     {
+//         //below is heap method takes time for memory allocation and delete
+//         /*
+//         ListNode* dummy = new ListNode(0);
+//         ListNode* curr = dummy;
+//         */
 
-        //stack one is prefer as it instantly
-        //Stack allocation is instantaneous, and it automatically cleans itself up, meaning no new and no delete.
-        ListNode dummy(0);
-        ListNode* curr = &dummy;
+//         //stack one is prefer as it instantly
+//         //Stack allocation is instantaneous, and it automatically cleans itself up, meaning no new and no delete.
+//         ListNode dummy(0);
+//         ListNode* curr = &dummy;
         
-        while(l1 && l2)
-        {    if(l1->val <= l2->val)
-            {
+//         while(l1 && l2)
+//         {    if(l1->val <= l2->val)
+//             {
                 
-                curr->next = l1;
-                l1 = l1->next;
-            }
-            else
-            {
-            curr->next = l2;
-            l2 = l2->next;
-            }
-            curr = curr->next;
-        }
-        if(l1) curr->next = l1;
-        else
-            curr->next =l2;
-        //below is heap method slow but SC is 1
-        /*
-        ListNode* newHead = dummy->next;
-        delete  dummy;
-        return newHead;
-        */
-        return dummy.next;  // since dummy not a pointer so it ListNode structure so dont use -> this we have to access the next of it with .
-    }
-    ListNode* sortList(ListNode* head) {
-        return sortLL(head);
-    }
-};
+//                 curr->next = l1;
+//                 l1 = l1->next;
+//             }
+//             else
+//             {
+//             curr->next = l2;
+//             l2 = l2->next;
+//             }
+//             curr = curr->next;
+//         }
+//         if(l1) curr->next = l1;
+//         else
+//             curr->next =l2;
+//         //below is heap method slow but SC is 1
+//         /*
+//         ListNode* newHead = dummy->next;
+//         delete  dummy;
+//         return newHead;
+//         */
+//         return dummy.next;  // since dummy not a pointer so it ListNode structure so dont use -> this we have to access the next of it with .
+//     }
+//     ListNode* sortList(ListNode* head) {
+//         return sortLL(head);
+//     }
+// };
 
 
 /*
@@ -95,3 +96,56 @@ REVISION NOTES
      access its members using the dot operator (`dummy.next`) instead of `->`. 
    - Use `&dummy` to create a pointer (`curr`) that traverses and builds the list.
 */
+
+class Solution {
+public:
+    ListNode* mergeLL(ListNode* l1, ListNode* l2)
+    {
+        ListNode dummy(0);
+        ListNode* curr = &dummy;
+        while(l1 && l2)
+        {
+            if(l1->val <= l2->val)
+                {
+                    curr->next = l1;
+                    l1 = l1->next;
+                }
+            else
+                {
+                    curr->next = l2;
+                    l2 = l2->next;
+                }
+            curr = curr->next;
+        }
+        if(l1)
+            curr->next = l1;
+        else
+            curr->next = l2;
+        return dummy.next;
+    }
+
+    ListNode* sortLL(ListNode* head)
+    {
+        if(!head || !head->next) return head;
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast && fast->next)
+            {
+                slow = slow->next;
+                fast = fast->next->next;
+            }
+        ListNode* rightHalf = slow->next;
+        slow->next = nullptr;
+        ListNode* leftHead = sortLL(head);
+        ListNode* rightHead = sortLL(rightHalf);
+        return mergeLL(leftHead, rightHead);
+    }
+
+    ListNode* sortList(ListNode* head)
+    {
+        return sortLL(head);
+    }
+
+
+
+};
