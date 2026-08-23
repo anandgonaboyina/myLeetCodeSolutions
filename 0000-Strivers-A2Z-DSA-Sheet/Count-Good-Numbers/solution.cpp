f3d66c6bdde10d0569f238d97632096e6e19df73
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-23 10:53:01 +0530

    Add solution for Count Good Numbers

--- 0000-Strivers-A2Z-DSA-Sheet/Count-Good-Numbers/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Count-Good-Numbers/solution.cpp
@@ -1,7 +1,22 @@
-//leetcode : 1922. Count Good Numbers
-class Solution {
+        long long ans = 1;
+    }
+    int countGoodNumbers(long long n) {
+        long long evenInd = n%2==0? n/2 : n/2 + 1;
+        long long oddInd = n/2;
+        long long MOD = 1e9 +7;
+        }
+        return ans;
+        {
+            if(n%2 == 1)
+                ans = (ans*x)%MOD;
+            x = (x*x)%MOD;
+            n /=2;
+        long long MOD = 1e9 + 7;
+        while(n>0)
+    long long modPow(long long x, long long n)
+    {
+        long long ans = 1;
+class Solution {
 public:
-    int countGoodNumbers(long long n) {
-        // Your code goes here
-    }
-};
+//optimal solution
+//got 100% beats
