a7bf584e62f020dde390412c0d7f1ba17a65b5e8
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-23 10:11:18 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.35%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,21 +1,46 @@
+//for SC 1
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+        if(N<0)
+        {
+            N = -1*N;
+            x = 1/x;
+        }
+        double ans=1.0;
+        while(N>0)
+        {
+            if(N%2 == 1 )
+                ans *= x;
+            x *=x;
+            N /=2;
+        }
+        return ans;
+    }
+
+};
+
 //revise 1
+//slightly modified code but same approach to achive SC as log2N due to recursive calls of binary division and TC also Log2N
+//but "old" way (using long long N = n) is actually the industry standard.
+/*
+Here is why: in production engineering, readability beats cleverness. Casting to a 64-bit integer (long long) takes zero extra execution time, uses practically zero extra memory, and allows other engineers to understand your code instantly without doing mental algebra of that INT_Min safety thing
+*/
+/*
 class Solution {
 public:
     double myPow(double x, int n) {
         if(n == 0) return 1.0;
         else if(n<0)
-            return (1/(x*myPow(x, -1*(n+1)) ));  // n+1 to avoid the INT_MIN convertion > INT_MAX
+            return (1/(x*myPow(x, -1*(n+1)) ));  
+            // n+1 to avoid the INT_MIN convertion > INT_MAX and for compensate it just multiply wtih x in denominator
         double half = myPow(x, n/2);
         if(n%2 == 0) return half*half;
         return half*half*x;
     }
 };
-
-
-
-
-
-
+*/
 
 //intial old approach
 /*
@@ -29,9 +54,7 @@ public:
     double solve(double x, long long n)
     {
         if(n<0)
-            {
-                return 1/solve(x, -1*n);
-            }
+                return 1/solve(x, -1*n);  // as here solve 2nd parameter is long long so INT_MIN can convert to INT_MAX in no time
         if(n==0)
             return 1;
         double half = solve(x, n/2);
