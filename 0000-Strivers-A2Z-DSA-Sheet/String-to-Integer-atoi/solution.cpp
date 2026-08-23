257b689ba66fd852fc435a72d6c86893b9b3c781
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-23 09:19:29 +0530

    Add solution for String to Integer (atoi)

--- 0000-Strivers-A2Z-DSA-Sheet/String-to-Integer-atoi/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/String-to-Integer-atoi/solution.cpp
@@ -1,46 +1,6 @@
-//leetcode problem : 8. String to Integer (atoi) 
-//my brute force solution  got 100 beats as Tc is N and Sc is 1
-// this is optimal solution
-/*
-class Solution {
-public:
-    int myAtoi(string s) {
-        long long ans =0;
-        bool isNeg = false;
-        int intMax = (1LL<<31)-1;   // use INT_MAX, INT_MIN built in leetcode varibles
-        int digitInd =0;
-        while(digitInd < s.size() && s[digitInd] == ' ')
-             digitInd++;
-        if(digitInd < s.size() && s[digitInd] == '-')
-            {
-                digitInd++;
-                isNeg = true;
-            }
-        else if(digitInd < s.size() && s[digitInd] == '+')
-            digitInd++;
-        for(int i=digitInd; i<s.size(); i++)
-        {
-            int n = s[i]-'0';
-            if(n >= 0 && n <= 9)
-                {
-                    if(ans*10 + n >= intMax)
-                        return intMax;
-                    else if (ans*10 - n <= INT_MIN)
-                        return INT_MIN;
-                    else if(isNeg)
-                        ans = ans*10 - n;
-                    else
-                        ans = ans*10 + n;
-                }
-            else
-                break;
-        }
-        return ans;
-    }
-};
-*/
+
 //revision 1
-//BETTER THAN FIRST APPROACH
+//BETTER THAN FIRST written code but same approach with minimized code
 class Solution {
 public:
     int myAtoi(string s) {
@@ -92,4 +52,48 @@ REVISION NOTES: String to Integer (myAtoi)
 5. Catch Overflow Inside the Loop:
    - A 30-digit string will overflow even a 'long long' if you wait until the loop finishes. 
    - Always check boundaries on the fly inside the while loop.
+*/
+
+
+//old attempts discarded no need to look at this
+
+//my brute force solution  got 100 beats as Tc is N and Sc is 1
+// this is optimal solution but not minimized code
+/*
+class Solution {
+public:
+    int myAtoi(string s) {
+        long long ans =0;
+        bool isNeg = false;
+        int intMax = (1LL<<31)-1;   // use INT_MAX, INT_MIN built in leetcode varibles
+        int digitInd =0;
+        while(digitInd < s.size() && s[digitInd] == ' ')
+             digitInd++;
+        if(digitInd < s.size() && s[digitInd] == '-')
+            {
+                digitInd++;
+                isNeg = true;
+            }
+        else if(digitInd < s.size() && s[digitInd] == '+')
+            digitInd++;
+        for(int i=digitInd; i<s.size(); i++)
+        {
+            int n = s[i]-'0';
+            if(n >= 0 && n <= 9)
+                {
+                    if(ans*10 + n >= intMax)
+                        return intMax;
+                    else if (ans*10 - n <= INT_MIN)
+                        return INT_MIN;
+                    else if(isNeg)
+                        ans = ans*10 - n;
+                    else
+                        ans = ans*10 + n;
+                }
+            else
+                break;
+        }
+        return ans;
+    }
+};
 */
