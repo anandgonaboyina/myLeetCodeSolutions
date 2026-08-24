298454a9c268952364ee011ad857720a55130892
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-24 17:21:56 +0530

    Add solution for Power Set

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Power-Set/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void solve(vector<int> &nums, vector<int> &pt, int i, vector<vector<int>> &ans)
+    {
+        if(i==nums.size())
+        {
+            ans.push_back(pt);
+            return;
+        }
+        pt.push_back(nums[i]);
+        solve(nums, pt, i+1, ans);
+        pt.pop_back();
+        solve(nums, pt, i+1, ans);
+    }
+    vector<vector<int> > powerSet(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        if(n==0) return ans;
+        vector<int> pt;
+        solve(nums, pt, 0, ans);
+        return ans;
+    }
+};
