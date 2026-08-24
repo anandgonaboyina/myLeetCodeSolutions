83025b09df2075ae4737637e7b0d28b1f5f5e0f8
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-24 13:53:18 +0530

    Add solution for Generate Parentheses

--- 0000-Strivers-A2Z-DSA-Sheet/Generate-Parentheses/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Generate-Parentheses/solution.cpp
@@ -1,20 +1,30 @@
-            curr.pop_back();
-            solve(n, open, close+1, curr, ans);
-            curr.push_back(')');
-        {
-        }
-        if(close < open)
-            curr.pop_back();
-            solve(n, open+1, close, curr, ans);
-            curr.push_back('(');
-        { 
-        if(open < n)
-            ans.push_back(curr);
-            return;
-        }
-        if(curr.size()== 2*n)
-        {
+class Solution {
 public:
-    void solve(int n, int open, int close, string &curr, vector<string> &ans)
-    {
-class Solution {
+    void solve(int n, int open, int close, string &curr, vector<string> &ans)
+    {
+        if(curr.size()== 2*n)
+        {
+            ans.push_back(curr);
+            return;
+        }
+        if(open < n)
+        { 
+            curr.push_back('(');
+            solve(n, open+1, close, curr, ans);
+            curr.pop_back();
+        }
+        if(close < open)
+        {
+            curr.push_back(')');
+            solve(n, open, close+1, curr, ans);
+            curr.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        if(n==0) return ans;
+        string s = "";
+        solve(n, 0, 0, s, ans);
+        return ans;
+    }
+};
