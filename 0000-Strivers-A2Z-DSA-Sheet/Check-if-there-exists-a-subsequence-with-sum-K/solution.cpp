c28b836945101b707c32d393bfff72d15a744ca2
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-25 18:37:45 +0530

    Add solution for Check if there exists a subsequence with sum K

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Check-if-there-exists-a-subsequence-with-sum-K/solution.cpp
@@ -0,0 +1,16 @@
+class Solution{
+    public:
+    bool solve(vector<int> &nums, int k, int sum, int i)
+    {
+        if(i==nums.size())
+        {
+            if(sum==k)
+                return true;
+            return false;
+        }
+        return solve(nums, k, sum+nums[i], i+1) || solve(nums, k, sum, i+1);
+    }
+    bool checkSubsequenceSum(vector<int>& nums, int k) {
+        return solve(nums, k, 0, 0);
+    }
+};
