bdf93f1c78dce2b1bf770300a7cc1e7b2c2cfd82
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-25 20:30:27 +0530

    Time: 4 ms (21.93%), Space: 13.9 MB (85%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,32 @@
+    class Solution {
+    public:
+
+        void solve(vector<int> &candidates, vector<vector<int>> &ans, vector<int> &vt, int target, int sum, int i)
+        {
+            if(i >= candidates.size() || sum>=target)
+            {
+                if(sum==target)
+                    ans.push_back(vt);
+                return;
+            }
+            vt.push_back(candidates[i]);
+            solve(candidates, ans, vt, target, sum+candidates[i], i+1);
+            vt.pop_back();
+            int curr=i;
+            while(i+1 < candidates.size() && candidates[curr]==candidates[i+1])
+                i++;
+            solve(candidates, ans, vt, target, sum, i+1);
+        }
+
+        vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+            vector<vector<int>> ans;
+            vector<int> vt;
+            sort(candidates.begin(), candidates.end());
+            solve(candidates, ans, vt, target, 0, 0);
+            return ans;
+        }
+    };
+
+
+    //note 
+    // as while loop takes us to after identical nums so we have to  go to the last identical one so i+1 works well
