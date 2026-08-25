02c0657ee869b74344a60b5e64cbe843460e77ee
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-25 23:13:11 +0530

    Add solution for Letter Combinations of a Phone Number

--- 0000-Strivers-A2Z-DSA-Sheet/Letter-Combinations-of-a-Phone-Number/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Letter-Combinations-of-a-Phone-Number/solution.cpp
@@ -1,13 +1,30 @@
 //same leetcode problem : 17. Letter Combinations of a Phone Number
 
+//got 100% betas 
 class Solution {
 public:
-    void solve()
+    void solve(string &digits, vector<string> &ans, string &mp, vector<string> &pad, int i)
+    {
+        if(i==digits.size())
+        {
+            ans.push_back(mp);
+            return;
+        }
+        for(int j=0; j<pad[digits[i]-'0'].size(); j++)
+        {
+            mp.push_back(pad[digits[i]-'0'][j]);
+            solve(digits, ans, mp, pad, i+1);
+            mp.pop_back();
+        }
+    }
+
     vector<string> letterCombinations(string digits) {
         vector<string> ans;
-        if(digits.empty()) return asn;
-        vector<string> pad = {'', '', "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        if(digits.empty()) return ans;
+        vector<string> pad = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         string mp;
+        solve(digits, ans, mp, pad, 0);
+        return ans;
 
     }
-};
+}; 
