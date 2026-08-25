d720ff4e2820b2e386b296e3bdc41a7b27f0ecec
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-25 23:41:38 +0530

    Add solution for Palindrome partitioning

--- 0000-Strivers-A2Z-DSA-Sheet/Palindrome-partitioning/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Palindrome-partitioning/solution.cpp
@@ -1,7 +1,33 @@
-//131. Palindrome Partitioning
+//131. Palindrome Partitioning
+//not completed need to solve this problem
+// ??????????????????????????????????????????????????????
+class Solution {
 public:
-    vector<vector<string> > partition(string s) {
-        
-    }
-class Solution {
+    void solve(string &s, vector<vector<string>> &ans, vector<string> &pt, int i)
+    {
+        if(i == s.size())
+        {
+            for(int k=0; k<pt.size(); k++)
+            {            
+                int l=0, r=pt[k].size();
+                while(l<r)
+                {
+                    if(pt[l] != pt[r])
+                        return;
+                    l++; r--;
+                }
+            }
+            ans.push_back(pt);
+            return;
+        }
+        vt.push_back()
+    }
+
+    vector<vector<string>>partition(string s) {
+        vector<vector<string>> ans;
+        if(s.empty()) return ans;
+        vector<string> pt;
+        solve(s, ans, pt, 0);
+        return ans;
+    }
 };
