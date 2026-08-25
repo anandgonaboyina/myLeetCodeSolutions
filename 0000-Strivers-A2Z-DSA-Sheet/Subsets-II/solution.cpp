633344fb85066314e144a3b9c1d985ac3517763b
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-25 21:43:03 +0530

    Modify subset function to handle duplicates
    Refactor the solution to handle subsets with duplicates and update function names for clarity.

--- 0000-Strivers-A2Z-DSA-Sheet/Subsets-II/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Subsets-II/solution.cpp
@@ -1,40 +1,10 @@
-//similar leetcode problem but it asks direct subsets return
-//78. Subsets
-class Solution {
-  public:
-    void solve(vector<int> &nums, vector<int> &ans, int sum, int i)
-    {
-        if(i==nums.size())
-        {
-            ans.push_back(sum);
-            return;
-        }
-        solve(nums, ans, sum+nums[i], i+1);
-        solve(nums, ans, sum, i+1);
-    }
-
-    vector<int> subsetSums(vector<int>& nums) {
-        vector<int> ans;
-        solve(nums, ans, 0, 0);
-        return ans;
-    }
-};
-
-//notes for above problem
-/*
-Because you must generate every single combination (there is no target sum to bound you), the Pick / Not-Pick template is the mathematically optimal way to solve this. Your time complexity is a perfect $O(2^N)
-=> One quick note: If the platform you are submitting to (like GeeksforGeeks) expects the final list of sums to be in increasing order, just add sort(ans.begin(), ans.end()); right before you return ans.
-*/
-
-//leetcode problem 78. Subsets
-//similar leetcode problem but it asks to return direct vector of subsets
-/*
+//leetcode same question
 //got 100% beats
 class Solution {
 public:
     void solve(vector<int> &nums, vector<vector<int>> &ans, vector<int> &vt, int i)
     {
-        if(i==nums.size())
+        if(i == nums.size())
         {
             ans.push_back(vt);
             return;
@@ -42,14 +12,17 @@ public:
         vt.push_back(nums[i]);
         solve(nums, ans, vt, i+1);
         vt.pop_back();
+        int curr = i;
+        while(i+1 < nums.size() && nums[curr] == nums[i+1])
+            i++;
         solve(nums, ans, vt, i+1);
     }
 
-    vector<vector<int>> subsets(vector<int>& nums) {
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> vt;
-        solve(nums, ans, vt, 0);
-        return ans;
+        sort(nums.begin(), nums.end());
+        solve(nums, ans, vt, 0); 
+        return ans; 
     }
 };
-*/
