98d71fd68bb1986875a47228b3762ea7b84f52e0
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-29 13:05:59 +0530

    Add solution for Check if a Number is Odd or Not

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Check-if-a-Number-is-Odd-or-Not/solution.cpp
@@ -0,0 +1,14 @@
+// class Solution {
+// public:
+//     bool isOdd(int n) {
+//        return n%2 !=0; 
+//     }
+// };
+
+// Bitwise AND 
+class Solution {
+public:
+    bool isOdd(int n) {
+       return n&1 == 1; // or // return n&1 !=0
+    }
+};
