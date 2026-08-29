206c9c2dbdb21ad37e557f58776cc01d78ce3180
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-29 13:38:19 +0530

    Time: 0 ms (100%), Space: 7.8 MB (82.02%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,18 +1,22 @@
-
-// one line bit manipulation math trick
-/*
-A power of two in binary always has exactly one 1 bit (e.g., 4 is 100, 8 is 1000).
-If you subtract 1 from a power of two, it flips that 1 and all the 0s after it (e.g., 4 - 1 = 3, which is 011).
-
-Because of this, doing a bitwise AND (&) between n and n - 1 will always equal 0 if the number is a power of two:
-100 & 011 == 000
-*/
+// got 100% beats as Tc is 1 and Sc is 1
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
         return n>0 && (n & (n-1) ) == 0;
     }
 };
+
+/* =================================
+Revision Notes
+=================================
+// one line bit manipulation math trick
+A power of two in binary always has exactly one 1 bit (e.g., 4 is 100, 8 is 1000).
+If you subtract 1 from a power of two, it flips that 1 and all the 0s after it (e.g., 4 - 1 = 3, which is 011).
+
+Because of this, doing a bitwise AND (&) between n and n - 1 will always equal 0 if the number is a power of two:
+100 & 011 == 000
+*/
+
 
 //old approach
 /*
