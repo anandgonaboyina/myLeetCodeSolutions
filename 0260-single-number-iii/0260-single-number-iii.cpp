e75903a55ea13f5910ce43b0cfa74b8c6d534d5c
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-29 19:25:40 +0530

    Time: 0 ms (100%), Space: 13.8 MB (81.32%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,4 +1,6 @@
 //got 100% beats
+//my optimal approach
+/*
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
@@ -26,9 +28,39 @@ public:
                     cnt++;
             }
             if(cnt%2 == 1)
-                ans[0] = ans[0] | 1<<i;
+                ans[0] = ans[0] | (1U <<i);
         }
         ans[1] = twoNums^ans[0];
+        return ans;
+    }
+};
+*/
+//other way in minimized code
+class Solution{
+public:
+    vector<int>  singleNumber(vector<int> &nums)
+    {
+        vector<int> ans(2, 0);
+        long long twoNums =0;
+        for(int num : nums)
+            twoNums ^= num;
+
+        int diffBit=0;
+        for(int i=0; i<32; i++)
+        {
+        if((twoNums>>i & 1 )== 1)
+            {
+                diffBit =i;
+                break;
+            }
+        }
+        for(int x : nums)
+        {
+            if((x>>diffBit & 1 )== 1)
+                ans[0] ^= x;
+            else
+                ans[1] ^= x;
+        }
         return ans;
     }
 };
