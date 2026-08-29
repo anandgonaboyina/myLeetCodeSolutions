5f29afa44b58830c59277c4f2017cb2f665b9946
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-29 14:24:16 +0530

    Add solution for Count the Number of Set Bits

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Count-the-Number-of-Set-Bits/solution.cpp
@@ -0,0 +1,26 @@
+//recursively
+class Solution {
+public:
+    int countSetBits(int n) {
+        if(n==0) return 0;
+        if((n & (n-1)) == 0) return 1;
+        return 1+countSetBits( n & (n-1));
+        // n & (n-1) jumps directly to the next set bit
+    }
+};
+//iteratively
+/*
+class Solution{
+public:
+    int countSetBits(int n)
+    {
+        int cnt =0;
+        int num = n;
+        while(num>0)
+        {
+            num = num & (num-1);
+            cnt++;
+        }
+    }
+};
+*/
