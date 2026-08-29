7cee36ea6f433fb452494fc81e922ab94df1f12c
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-29 18:50:47 +0530

    Time: 0 ms (100%), Space: 13.4 MB (35.46%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans =0;
+        vector<int> freq(31, 0);
+        for(int i=0; i<=31; i++)
+        {
+        int cnt =0;
+        for(int num : nums)
+           {
+            if((num >>i & 1) == 1)
+                cnt++;
+           }
+           if(cnt%3 == 1 )
+                ans = ans | (1<<i);
+        }
+        return ans;
+    }
+};
