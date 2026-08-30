dce20caf784d7db6852c4e1c5e21cec6b0bc202a
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-30 18:56:58 +0530

    Time: 0 ms (100%), Space: 9.8 MB (82.26%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,4 +1,6 @@
+//using the recursion
 //got 100% beats
+/*
 class Solution {
 public:
     void solve(vector<int> &nums, vector<vector<int>> &ans, vector<int> &vt, int i)
@@ -18,6 +20,27 @@ public:
         vector<vector<int>> ans;
         vector<int> vt;
         solve(nums, ans, vt, 0);
+        return ans;
+    }
+};
+*/
+
+//using the bit manipulation
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        for(int i=0; i<(1<<n); i++)
+        {
+        vector<int> sub;
+            for(int j=0; j<n; j++)
+            {
+                if((i>>j & 1) == 1)
+                    sub.push_back(nums[j]);
+            }
+        ans.push_back(sub);
+        }
         return ans;
     }
 };
