897f1dfe4f888df3e52b3cf4da4d66245091273e
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-31 19:33:18 +0530

    Add solution for Balanced Paranthesis

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Balanced-Paranthesis/solution.cpp
@@ -0,0 +1,25 @@
+//leetcode : 20. Valid Parentheses
+//got 100% beats SC is N and TC is N
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        if(n==0) return true;
+        stack<char> st;
+        for(char x : s)
+        {
+            if(x == '(' || x == '{' || x == '[')
+                st.push(x);
+            else
+            {
+                if(st.empty())
+                    return false;
+                if( (x == ')' &&  st.top() == '(') || (x == ']' &&  st.top() == '[') || (x == '}' &&  st.top() == '{') )
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+        return st.empty();
+    }
+};
