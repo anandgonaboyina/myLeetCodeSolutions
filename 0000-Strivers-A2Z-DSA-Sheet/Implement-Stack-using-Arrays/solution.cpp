b1a62e4681e0709f48b5f0d27f5c82d711050fba
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-31 07:42:41 +0530

    Add solution for Implement Stack using Arrays

---
+++ 0000-Strivers-A2Z-DSA-Sheet/Implement-Stack-using-Arrays/solution.cpp
@@ -0,0 +1,20 @@
+        stackArray[++topIndex] = x;
+    }
+            cout<<"stack overflow"<<endl;
+            return;
+        }
+        if(top >= capacity - 1;)
+        {
+    
+    void push(int x) {
+        stackArray = new int[capacity];
+        topIndex = -1;
+    }
+    ArrayStack(int size = 1000) {
+        capacity = size;
+int capacity;
+int topIndex;
+public:
+privte:
+int * stackArray;
+class ArrayStack {
