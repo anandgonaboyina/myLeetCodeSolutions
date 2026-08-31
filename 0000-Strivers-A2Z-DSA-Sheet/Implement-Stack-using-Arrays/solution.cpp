90088e9d6ded0073b8d1a9d61f9836307924226d
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-31 07:50:34 +0530

    Add solution for Implement Stack using Arrays

--- 0000-Strivers-A2Z-DSA-Sheet/Implement-Stack-using-Arrays/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Implement-Stack-using-Arrays/solution.cpp
@@ -1,20 +1,47 @@
-        stackArray[++topIndex] = x;
-    }
-            cout<<"stack overflow"<<endl;
-            return;
-        }
-        if(top >= capacity - 1;)
-        {
-    
-    void push(int x) {
-        stackArray = new int[capacity];
-        topIndex = -1;
-    }
-    ArrayStack(int size = 1000) {
-        capacity = size;
-int capacity;
-int topIndex;
-public:
+class ArrayStack {
 privte:
-int * stackArray;
-class ArrayStack {
+int * stackArray;
+int capacity;
+int topIndex;
+public:
+    ArrayStack(int size = 1000) {
+        capacity = size;
+        stackArray = new int[capacity];
+        topIndex = -1;
+    }
+    
+    void push(int x) {
+        if(top >= capacity - 1;)
+        {
+            cout<<"stack overflow"<<endl;
+            return;
+        }
+        stackArray[++topIndex] = x;
+    }
+    
+    int pop() {
+        if(topIndex == -1 || isEmpty())
+        {
+            cout<<"stack underflow"<<endl;
+            return -1;
+        }
+        return stackArray[topIndex--];
+    }
+    
+    int top() {
+    if(topIndex == -1 || isEmpty())
+    {
+        cout<<"stack underflow"<<endl;
+        return -1;
+    }
+    return stackArray[topIndex];
+    }
+    
+    bool isEmpty() {
+       return (top == -1)
+    }
+    int size()
+    {
+        return topIndex+1;
+    }
+};
