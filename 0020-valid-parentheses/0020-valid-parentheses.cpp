1623723d3ba01bfdf0f3559f58f2f87b18aa7d7a
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-31 19:31:55 +0530

    Time: 1 ms (18.39%), Space: 8.9 MB (37.4%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.size();
+        if(n==0) return true;
+        stack<char> st;
+        for(char x : s)
+        {
+            if(x == '(' || x == '{' || x == '[')
+                st.push(x);
+            else
+            {
+                if(st.empty())
+                    return false;
+                if( (x == ')' &&  st.top() == '(') || (x == ']' &&  st.top() == '[') || (x == '}' &&  st.top() == '{') )
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+        return st.empty();
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//old tried not done
+/*
+
+class Solution {
+public:
+    bool isValid(string s) {
+        int l = 0;
+        int r = s.length()-1;
+        while(l<r)
+        {
+            if((s[l] !=s[r])) 
+                return false;
+            l++; r--;
+        }
+        return true;
+    }
+};
+*/
