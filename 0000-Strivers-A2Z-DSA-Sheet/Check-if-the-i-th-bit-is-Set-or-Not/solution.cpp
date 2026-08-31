de96ab4270c27cc721b01568c98037e306533efe
anandgonaboyina <anandgonaboyina@gmail.com>
2026-08-31 20:03:31 +0530

    Add solution for Check if the i-th bit is Set or Not

--- 0000-Strivers-A2Z-DSA-Sheet/Check-if-the-i-th-bit-is-Set-or-Not/solution.cpp
+++ 0000-Strivers-A2Z-DSA-Sheet/Check-if-the-i-th-bit-is-Set-or-Not/solution.cpp
@@ -1,8 +1,25 @@
+//optimal approach
+/*
 class Solution {
 public:
     bool checkIthBit(int n, int i) {
-        bool status = (n>>i & 1)==1? true : false;
-        // or // bool status = (1<<i) & n == 1? true : false;
-        return status;
+       
+        return  (n>>i & 1)==1;  //or bit mask way (1<<i) & n == 1;
+    }
+};
+*/
+//brute force
+class Solution {
+public:
+    bool checkIthBit(int n, int i) {
+       string binary = "";
+       while(n>0)
+       {
+            binary += n%2 == 0 ? '0':'1';
+            n = n/2;
+       }
+       if(i >= binary.size())
+            return false;
+        return  binary[i]=='1';
     }
 };
